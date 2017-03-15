7520c3feb4106c4205f2f2cb6d0ce093504ecdab
alexandreuch <alexandreuch@gmail.com>
2017-03-15 19:16:10 -0400

    Add files via upload

---
+++ lista1.5/lista1.5_questao7_b.py
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------------------------------------------------
+#Introdução a Programação de Computadores - IPC
+#Universidade do Estado do Amazonas - UEA
+#Prof. Jucimar Jr.
+#Alexandre Marques Uchôa               1715310028
+#Jandinne Duarte de Oliveira           1015070265
+#Uriel Brito Barros                    1515120558
+#Roberta de Oliveira da cruz           0825070169
+#Evandro Padilha Barroso Filho         1715310009
+#
+##
+#Calcule o novo peso se a pessoa emagrecer 20% sobre o peso digitado
+#-----------------------------------------------------------------------------------------------------------------------
+weight = float(input('Digite o seu peso em quilogramas'))
+lost_weight = (weight*0.80)
+print ('Seu peso irá ser', lost_weight)
