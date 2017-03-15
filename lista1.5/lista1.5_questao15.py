672b636f4cb29b7f0294426c5865767d377729bd
cetdo <cetdo@outlook.com>
2017-03-15 18:58:37 -0400

    Criando o arquivo da questão 15

---
+++ lista1.5/lista1.5_questao15.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Carlos Eduardo Tapudima de Oliveira       1715310030
+# Frederico Victor Alfaia Rodrigues         1515200030
+# Joelson Pereira Lima	                    1715310060
+# Lucas Gabriel Silveira Duarte             1715310053
+# Reinaldo da Silva Vargas	            1715310054
+# Walter Nobre da Silva Conceição           1715310057
+#
+# 20 - João recebeu seu salário e precisa pagar duas contas que estão
+# atrasadas, João terá de pagar multa de 2% sobre cada conta. Faça um programa
+# que calcule e mostre quanto restará do salário de João.
+# ----------------------------------------------------------
