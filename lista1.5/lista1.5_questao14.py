a2123684e2122e42207851a81a9f90fc1b913d21
cetdo <cetdo@outlook.com>
2017-03-15 19:38:06 -0400

    colocando a questão nos padrões estabelecidos

--- lista1.5/lista1.5_questao14.py
+++ lista1.5/lista1.5_questao14.py
@@ -20,18 +20,17 @@
 
 print ('---------------------Cálculo da Idade---------------------')
 try:
-    a = input ('Digite o ano atual: ')
-    a = int(a)
-    b = input ('Digite o ano de nascimento: ')
-    b = int(b)
-    c = a - b
-    d = c * 12
-    e = c * 365
-    f = c * 48
-    print ('A soma: ',a,'  - ',b,' é igual a : ',c,' que é a idade da pessoa em anos')
-    print(' A multiplicação ',c,' * ',12,' é igual a : ',d,' que é a idade da pessoa em meses')
-    print(' A multiplicação ',c,' * ',365,' é igual a : ',e,' que é a idade da pessoa em dias')
-    print('A multiplicação ',c,' * ',48,' é igual a: ',f,' que é a idade da pessoa em semanas')
+    running_year = input ('Digite o ano atual: ')
+    running_year = int(running_year)
+    birth_year = input ('Digite o ano de nascimento: ')
+    birth_year = int(birth_year)
+    age = running_year - birth_year
+    age_in_months = age * 12
+    age_in_days = age * 365
+    age_in_weeks = age * 48
+    print (' A subtração',running_year,'  - ',birth_year,' é igual a : ',age,' que é a idade da pessoa em anos')
+    print(' A multiplicação ',age,' * ',12,' é igual a : ',age_in_months,' que é a idade da pessoa em meses')
+    print(' A multiplicação ',age,' * ',365,' é igual a : ',age_in_days,' que é a idade da pessoa em dias')
+    print(' A multiplicação ',age,' * ',48,' é igual a: ',age_in_weeks,' que é a idade da pessoa em semanas')
 except ValueError:
-    print('Somente números são aceitos, tente novamente!')
-
+    print(' Somente números são aceitos, tente novamente!')
