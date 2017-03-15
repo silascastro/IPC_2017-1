0d537f6ceddbcbee8cb1d6c2d9b571cde6a4e440
LucaSilveira <lucas.gabrielsd@gmail.com>
2017-03-15 19:27:49 -0400

    cabeçalho e correções

--- lista1.5/lista1.5_questao14.py
+++ lista1.5/lista1.5_questao14.py
@@ -1,4 +1,22 @@
-print ('---------------------Subtração de 2 números---------------------')
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Carlos Eduardo Tapudima de Oliveira       1715310030
+# Frederico Victor Alfaia Rodrigues         1515200030
+# Joelson Pereira Lima                      1715310060
+# Lucas Gabriel Silveira Duarte             1715310053
+# Reinaldo da Silva Vargas                  1715310054
+# Walter Nobre da Silva Conceição           1715310057
+#
+# 14. Faça um programa que receba o ano de nascimento de uma pessaoa e o ano atual, calcule e mostre:
+# a) a idade dessa pessoa em anos;
+# b) a idade dessa pessoa em meses;
+# c) a idade dessa pessoa em dias;
+# d) a idade dessa pessoa em semanas. 
+
+print('---------------------Subtração de 2 números---------------------')
 try:
     a = input ('Digite o ano atual: ')
     a = int(a)
@@ -8,7 +26,7 @@ try:
     d = c * 12
     e = c * 365
     f = c * 48
-    print ('A soma: ',a,'  - ',b,' é igual a : ',c,' que é a idade da pessoa em anos')
+    print ('A subtração: ',a,'  - ',b,' é igual a : ',c,' que é a idade da pessoa em anos')
     print(' A multiplicação ',c,' * ',12,' é igual a : ',d,' que é a idade da pessoa em meses')
     print(' A multiplicação ',c,' * ',365,' é igual a : ',e,' que é a idade da pessoa em dias')
     print('A multiplicação ',c,' * ',48,' é igual a: ',f,' que é a idade da pessoa em semanas')
