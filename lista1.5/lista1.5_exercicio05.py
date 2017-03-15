58d4cd18b9c38cf60e5168cafcb8dfa7ac1da3d9
urielbarrros <urielbbarros@gmail.com>
2017-03-15 18:57:14 -0400

    enviando questão #5

---
+++ lista1.5/lista1.5_exercicio05.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#Alexandre Marques Uchôa               1715310028
+#Jandinne Duarte de Oliveira           1015070265
+#Uriel Brito Barros                    1515120558
+#Roberta de Oliveira da cruz           0825070169
+#Evandro Padilha Barroso Filho         1715310009
+#2. Faça um programa que receba o preço de um produto, calcule e mostre o novo preço, sabendo-se que este sofreu um desconto de 10%
+# ----------------------------------------------------------
+price = float(input('digite o preço do produto '))
+newvalor = price * 0.9
+print ('o novo valor do produto eh ', newvalor)
