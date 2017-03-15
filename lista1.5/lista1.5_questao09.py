a922e05c58ca9736dc009ed34499f15067ba4007
natcavalcantex21 <nataliacxavier1@gmail.com>
2017-03-15 19:19:27 -0400

    Add files via upload

---
+++ lista1.5/lista1.5_questao09.py
@@ -0,0 +1,16 @@
+#+#-------------------------------------------------------------------
+ #Introdução a Programação de Computadores-IPC
+ #Profº Jucimar Jr
+ #Natália Cavalcante Xavier  1715310021
+ #Antonio Diego Furtado da Silva 1715310004
+ #Mateus Oliveira Marques 1515310514
+ #Judá Salazar Braga 1515200050
+ #Fang Yao 1115180236
+ #Vitor Simoes Azevedo 1715310025
+ #Faça um Programa que calcule e mostra a área de um trapézio
+ #--------------------------------------------------------------------------------------------------------
+bmaior=float(input("Digite o valor da base maior "))
+bmenor=float(input("Digite o valor da base menor "))
+h=float(input("Digite o valor da altura"))
+area=(bmaior+bmenor)*h/2
+print("a area é ",area)
