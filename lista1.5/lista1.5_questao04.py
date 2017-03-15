49783346a86ce146fa8abebc21f592607a65ae65
EnriqueIzel2 <eleaoizel8@gmail.com>
2017-03-15 19:04:07 -0400

    Update lista1.5_questao04.py

--- lista1.5/lista1.5_questao04.py
+++ lista1.5/lista1.5_questao04.py
@@ -1,3 +1,15 @@
+#---------------------------------------------------------------------
+#Introdução a Programação dos Computadores - IPC
+#Universidade do Estado do Amazonas - UEA
+#Prof. Jucimar Jr.
+#Adham Lucas da Silva Oliveira           1715310001
+#Enrique Leão Barbosa Izel               1715310048
+#Erik Atilio Silva Rey                   1715310059
+#Ulisses Antonio Antonino da Costa       1515090555
+#Guilherme Silva de Oliveira             1715310034
+#Faça um programa que receba duas notas, calcule e mostre a média ponderada dessas duas notas, 
+#considerando peso 2 para a primeira nota e peso 3 para a segunda nota
+#----------------------------------------------------------------------
 note1= float(input('primeira nota'))
 note2= float(input('segunda nota'))
 average= (note1*2 + note2*3) / 5
