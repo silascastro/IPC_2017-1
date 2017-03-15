029c76fd887b5363aec77e313e6020caf73f331c
LukasMS <lukasmichel66@gmail.com>
2017-03-15 19:29:42 -0400

    Add files via upload

---
+++ lista1.5/lista1.5_questao01.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Lukas Michel Souza Mota	1715310018
+#
+# Faça um programa que receba dois números,
+# clacule e mostre a subtração do primeiro pelo segundo
+# ----------------------------------------------------------
+
+a = int(input("Digite o valor de a:"))
+b = int(input("Digite o valor de b:"))
+
+print("a - b = %d" % (a - b))
