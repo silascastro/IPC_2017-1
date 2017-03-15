77b19aeadebc542a95c190c727f40373cfc672de
Guilherme Oliveira <guilhermesilva609@gmail.com>
2017-03-15 19:34:19 -0400

    Update lista1.5_questao01.py

--- lista1.5/lista1.5_questao01.py
+++ lista1.5/lista1.5_questao01.py
@@ -1,9 +1,12 @@
-# ----------------------------------------------------------
-# Introdução a Programação de Computadores - IPC
-# Universidade do Estado do Amazonas - UEA
-# Prof. Jucimar Jr
-#
-# Lukas Michel Souza Mota	1715310018
+#---------------------------------------------------------------------
+#Introdução a Programação dos Computadores - IPC
+#Universidade do Estado do Amazonas - UEA
+#Prof. Jucimar Jr.
+#Adham Lucas da Silva Oliveira           1715310001
+#Enrique Leão Barbosa Izel               1715310048
+#Erik Atilio Silva Rey                   1715310059
+#Ulisses Antonio Antonino da Costa       1515090555
+#Guilherme Silva de Oliveira             1715310034
 #
 # Faça um programa que receba dois números,
 # clacule e mostre a subtração do primeiro pelo segundo
