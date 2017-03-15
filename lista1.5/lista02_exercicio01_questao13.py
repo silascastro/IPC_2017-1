84c31c5179f5d263dc87314bf2d3a305ce30a96b
yurileandro <yurileandro0123@outlook.com>
2017-03-15 19:48:11 -0400

    Grupo 04
    Questão 13 (falta concluir)

---
+++ lista1.5/lista02_exercicio01_questao13.py
@@ -0,0 +1,21 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Aracille de Souza Barbosa           1315120206
+# Felipe Eduardo Silva de Almeida     1715310031
+# Kethelen Tamara Braga               1525212002
+# Nayara da Silva Cedeira da Costa    1715310038
+# Vitor Summer Oliveira Pantaleão     1715310042
+# Yuri Leandro de Aquino Silva        1615100462
+#
+tab=input("Qual a tabuada que você quer?")
+num=int(input("De quanto?"))
+if((tab=="x")or(tab=="vezes")):
+    for x in range(11):
+        resp=num*x
+        print(resp)
+elif((tab=="-")or(tab=="menos")):
+    for x in range(11):
+        resp=num-x
+        print(resp)
