882ccb6b4c05f6be84de3723c91b7e071edb05db
Amanda <a.amandachaves@outlook.com>
2017-03-15 19:13:41 -0400

    Subindo questao

---
+++ lista1.5/lista1.5_questao08.py
@@ -0,0 +1,18 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Antonio Diego Furtado da Silva        1715310004
+# Fang Yao                              1115180236
+# Juda Salazar Braga                    1515200050
+# Matheus de Oliveira Marques           1515310514
+# Vitor Simoes Azevedo                  1715310025
+# Natalia Cavalcante Xavier             1715310021
+# Receber o peso de uma pessoa em quilos e converter para gramas
+# ----------------------------------------------------------
+
+q = float(input("Digite o peso em quilos: "))
+
+g = q*1000
+
+print("Seu peso em gramas é: ",g)
