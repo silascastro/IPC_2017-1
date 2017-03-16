9eeea80bfc3b225a384cdb0941ca107d1f71c1b6
tiagoweb <tiagoweb@gmail.com>
2017-03-15 20:49:43 -0400

    formatação / padronização

--- lista1.5/lista1.5_questao20.py
+++ lista1.5/lista1.5_questao20.py
@@ -5,7 +5,7 @@
 # Edson de Lima Barros                      1715310043
 # Gabriel Nascimento de Oliveira            1715310052
 # Luiz Daniel Raposo Nunes de Mello	        1715310049
-# Tiago Ferreira Aranha 	                1715310047
+# Tiago Ferreira Aranha 	                  1715310047
 # Wilbert Luís Evangelista Marins           1715310055
 # Mackson Garcez Moreno de Oliveira júnior  1215090300
 #
@@ -13,6 +13,7 @@
 # e a distância que a escada está da parede. Calcule e mostre a medida da escada para que se
 # possa alcançar a ponta da escada.
 # ----------------------------------------------------------
+
 angle = int(input('informe o angulo'))
 distance = int(input('informe a distancia'))
 
