650c187f89d693aea40a1f902757f80c13af9777
tiagoweb <tiagoweb@gmail.com>
2017-03-15 20:51:37 -0400

    Update lista1.5_questao22.py

--- lista1.5/lista1.5_questao22.py
+++ lista1.5/lista1.5_questao22.py
@@ -5,13 +5,17 @@
 # Edson de Lima Barros                      1715310043
 # Gabriel Nascimento de Oliveira            1715310052
 # Luiz Daniel Raposo Nunes de Mello	        1715310049
-# Tiago Ferreira Aranha 	                1715310047
+# Tiago Ferreira Aranha 	                  1715310047
 # Wilbert Luís Evangelista Marins           1715310055
 # Mackson Garcez Moreno de Oliveira júnior  1215090300
 #
-# 22. Faça um programa que receba o número de lados de um polígono convexo, calcule e mostre o número de diagonais desse polígono,
+# 22. Faça um programa que receba o número de lados de um 
+# polígono convexo, calcule e mostre o número de diagonais desse polígono,
 # onde N é o número de lados do polígono. Sabe-se que ND=N(N-3)/2.
-#
+# ----------------------------------------------------------
+
 numbers_of_sides = int(input('informe o numero de lados'))
+
 n_diagonals = (numbers_of_sides*(numbers_of_sides-3))/2
+
 print(n_diagonals)
