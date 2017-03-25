dc2efca952425d0d7e1748c5b6655481885a7e7c
Wesley Rocha <wesleysr1997@gmail.com>
2017-03-25 15:07:50 -0400

    Correção de lista04_lista01_questao07.py

--- lista04/lista04_lista01_questao07.py
+++ lista04/lista04_lista01_questao07.py
@@ -1,3 +1,18 @@
+#------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr.
+#
+# Vitor Summer Oliveira Pantaleão 	1715310042
+# Vítor Simões Azevedo 			1715310025
+# Wesley da Silva Rocha			1715310026
+# Wilbert Luís Evangelista Marins 	1715310055
+# Yuri Leandro de Aquino Silva 		1615100462
+#
+# Faça um Programa que leia três números
+# e mostre o maior e o menor deles.
+#------------------------------------------------------------------------------
+
 n1 = float(input('Digite um numero: '))
 n2 = float(input('Digite um numero: '))
 n3 = float(input('Digite um numero: '))
