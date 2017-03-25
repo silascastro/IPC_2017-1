fec72ee8f2d8b3523c73f8a839499fc0a1e4cd19
victorHuguw <kibedasarabia@gmail.com>
2017-03-25 12:48:21 -0400

    ass

---
+++ lista02_exercicio02.py
@@ -0,0 +1,4 @@
+#2)2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+#
+number = int(input("digite um numero:"))
+print("o numero digitado foi:", number)
