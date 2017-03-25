516d86a515cfff46b2279cce4227b25f4c61374b
VictorHuguw <kibedasarabia@gmail.com>
2017-03-25 12:49:47 -0400

    Delete lista02_exercicio02.py

--- lista02_exercicio02.py
+++
@@ -1,4 +0,0 @@
-#2)2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-#
-number = int(input("digite um numero:"))
-print("o numero digitado foi:", number)
