b11b42d8f6c904541ce136f5a13f7ce1ce47c205
evandro7047301 <evandro7047301@gmail.com>
2017-03-26 00:33:44 -0400

    Ajustando cabeçalho - Grupo02

--- lista04/lista04_lista02_questao22.py
+++ lista04/lista04_lista02_questao22.py
@@ -1,3 +1,22 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC                                                                        
+# Universidade do Estado do Amazonas - UEA                                                                              
+# Prof. Jucimar Jr.                                                                                                     
+# Erik Atlio Silva Rey                  1715310059                                                                      
+# Edson de Lima Barros                  1715310043                                                                      
+# Enrique Leão Barbosa Izel             1715310048                                                                      
+# Evandro Padilha Barroso Filho         1715310009                                                                      
+# Fang Yao                              1115180236                                                                      
+#                                                                                                                       
+#22.Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com 
+#D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem       
+#"Valores aceitos", senão escrever "Valores nao aceitos".                                                               
+#Entrada                                                                                                                
+#Quatro números inteiros A, B, C e D.                                                                                   
+#Saída                                                                                                                  
+#Mostre a respectiva mensagem após a validação dos valores.                                                             
+#-----------------------------------------------------------------------------------------------------------------------
+                                                                                                                        
 a, b, c, d = input().split()
 A = float(a)
 B = float(b)
