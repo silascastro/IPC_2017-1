febd9ead8460c3ea97adedcf408ef21dc9587bfa
evandro7047301 <evandro7047301@gmail.com>
2017-03-26 12:55:38 -0400

    fazendo up da questao02 lista 02 grupo02

---
+++ lista04/lista04_lista02_questao52.py
@@ -0,0 +1,62 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr.
+# Erik Atlio Silva Rey                  1715310059
+# Edson de Lima Barros                  1715310043
+# Enrique Leão Barbosa Izel             1715310048
+# Evandro Padilha Barroso Filho         1715310009
+# Fang Yao                              1115180236
+#
+#52.Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês
+#do ano por extenso, em inglês, com a primeira letra maiúscula.
+#Entrada
+#A entrada contém um único valor inteiro.
+#Saída
+#Imprima por extenso o nome do mês correspondente ao número existente na entrada, com a primeira letra em maiúscula.
+#-----------------------------------------------------------------------------------------------------------------------
+
+month = int(input())
+if month == 1:
+    print("January")
+if month == 2:
+    print("February")
+if month == 3:
+    print("March")
+if month == 4:
+    print("April")
+if month == 5:
+    print("May")
+if month == 6:
+    print("June")
+if month == 7:
+    print("July")
+if month == 8:
+    print("August")
+if month == 9:
+    print("September")
+if month == 10:
+    print("October")
+if month == 11:
+    print("November")
+if month == 12:
+    print("December")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
