d0935d2cb157c0afe28a00664a19319f015c6dd1
erikatilio <erikatilio@users.noreply.github.com>
2017-03-27 08:51:08 -0400

    Ajustando cabeçalho

--- lista04/lista04_lista02_questao12.py
+++ lista04/lista04_lista02_questao12.py
@@ -1,3 +1,27 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr.
+# Erik Atlio Silva Rey                  1715310059
+# Edson de Lima Barros                  1715310043
+# Enrique Leão Barbosa Izel             1715310048
+# Evandro Padilha Barroso Filho         1715310009
+# Fang Yao                              1115180236
+#
+#12.Escreva um programa que leia três valores com ponto flutuante de dupla precisão:
+# A, B e C. Em seguida, calcule e mostre:
+#a) a área do triângulo retângulo que tem A por base e C por altura.
+#b) a área do círculo de raio C. (pi = 3.14159)
+#c) a área do trapézio que tem A e B por bases e C por altura.
+#d) a área do quadrado que tem lado B.
+#e) a área do retângulo que tem lados A e B.
+#Entrada
+#O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+#Saída
+#O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a uma das áreas descritas acima,
+#sempre com mensagem correspondente e um espaço entre os dois pontos e o valor.
+#O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.
+#-----------------------------------------------------------------------------------------------------------------------
 
 value = input().split()
 
