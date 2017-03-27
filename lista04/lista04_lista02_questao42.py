9ab21a08ca40c44ac4cd1a1274c2fdd825c1acd1
erikatilio <erikatilio@users.noreply.github.com>
2017-03-27 08:59:47 -0400

    Ajustando padrões

--- lista04/lista04_lista02_questao42.py
+++ lista04/lista04_lista02_questao42.py
@@ -10,9 +10,9 @@
 #
 #42.Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, uma linha em
 # branco e em seguida, os valores na sequência como foram lidos.
-#Entrada
+#Entrada:
 #A entrada contem três números inteiros.
-#Saída
+#Saída:
 #Imprima a saída conforme foi especificado.
 #-----------------------------------------------------------------------------------------------------------------------
 
