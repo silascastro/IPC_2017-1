6abd8edf9615d4ec9af8e55a29e1063919545763
erikatilio <erikatilio@users.noreply.github.com>
2017-03-27 08:58:02 -0400

    grupo 02

--- lista04/lista04_lista02_questao12.py
+++ lista04/lista04_lista02_questao12.py
@@ -15,9 +15,9 @@
 #c) a área do trapézio que tem A e B por bases e C por altura.
 #d) a área do quadrado que tem lado B.
 #e) a área do retângulo que tem lados A e B.
-#Entrada
+#Entrada:
 #O arquivo de entrada contém três valores com um dígito após o ponto decimal.
-#Saída
+#Saída:
 #O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a uma das áreas descritas acima,
 #sempre com mensagem correspondente e um espaço entre os dois pontos e o valor.
 #O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.
