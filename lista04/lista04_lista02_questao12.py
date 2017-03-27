246e7c2eaaccbcc559d1bf1f74dded5348660a9b
evandro7047301 <evandro7047301@gmail.com>
2017-03-27 09:27:19 -0400

    ajustanto nomes de variáveis

--- lista04/lista04_lista02_questao12.py
+++ lista04/lista04_lista02_questao12.py
@@ -29,14 +29,14 @@ a, b, c = value
 pi = 3.14159
 
 triangle = (float(a) * float(c))/2
-circule = pi * (float(c)* float(c))
+circle = pi * (float(c)* float(c))
 trapezius = float(c) *(float(a) + float(b)) / 2
 square = float(b) * float(b)
 rectangle = float(a) * float(b)
 
 
 print("TRIANGULO: %0.3f"%triangle)
-print("CIRCULO: %0.3f"%circule)
+print("CIRCULO: %0.3f"%circle)
 print("TRAPEZIO: %0.3f"%trapezius)
 print("QUADRADO: %0.3f"%square)
 print("RETANGULO: %0.3f"%rectangle)
