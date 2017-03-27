40fe008d5f6a53eed457cf435278b60c9e7bd194
erikatilio <erikatilio@users.noreply.github.com>
2017-03-27 09:04:46 -0400

    grupo 02

--- lista04/lista04_lista02_questao02.py
+++ lista04/lista04_lista02_questao02.py
@@ -16,7 +16,7 @@
 -#Saída:
 -#Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo,
 -#com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas,
--#não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+-#não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá ("Presentation Error").
 -#------------------------------------------------------------------------------------------------------------
 pi = 3.14159
 radius = float(input())
