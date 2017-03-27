8ee15382a4b2d53682ff690787cc6ac3bb32f674
marcuspaess <mvpdss.eng@uea.edu.br>
2017-03-27 00:05:11 -0400

    Subindo questão 1007

---
+++ lista04/lista04_lista02_questão07.py
@@ -0,0 +1,21 @@
+#--------------------------------------------------------------------------
+#Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+
+#Reinaldo da Silva Varas 1715310054
+#Natalia Cavalcante Xavier 1715310021
+#Nayara da Silva Cerdeira da Costa 1715310038
+#Marcus Vinicius Paes da Silva Santos 1515070060
+#Matheus de Oliveira Marques 1515310514
+#Criar um programa que calcule e imprima a diferença do produto A e B pelo produto de C e D.
+#-------------------------------------------------------------------------------------
+
+A = int(input('Digite um número para A '))
+B = int(input('Digite um número para B '))
+C = int(input('Digite um número para C '))
+D = int(input('Digite um número para D '))
+DIFERENCA = (A*B-C*D)
+
+print ('DIFERENCA =', DIFERENCA)
+
