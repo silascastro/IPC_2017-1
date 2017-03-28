15fdef9b9f97313bfb7ab26acf6ee150e535939c
KethelenTamara <kettamara@gmail.com>
2017-03-28 13:50:56 -0300

    Update lista04_lista01_questao03.py

--- lista04/lista04_lista01_questao03.py
+++ lista04/lista04_lista01_questao03.py
@@ -8,6 +8,8 @@
 # Judá Salazar Braga                    1515200050
 # Kethelen Tamara Braga Barbosa         1525212002
 # Lucas Gabriel Silveira                1715310053
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - 
+#Masculino, Sexo Inválido.
 
 
 print("Digite M para masculino e F para feminino")
