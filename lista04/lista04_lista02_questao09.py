d374aa7c2e51d922480b61ab7d964a439069ea48
Wesley Rocha <wesleysr1997@gmail.com>
2017-03-28 12:11:09 -0400

    Adicionado lista04_lista02_questao09.py

---
+++ lista04/lista04_lista02_questao09.py
@@ -0,0 +1,20 @@
+#------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr.
+#
+# Vitor Summer Oliveira Pantaleão   1715310042
+# Vítor Simões Azevedo          1715310025
+# Wesley da Silva Rocha         1715310026
+# Wilbert Luís Evangelista Marins   1715310055
+# Yuri Leandro de Aquino Silva      1615100462
+#
+# Faça um Programa que peça um número correspondente a um determinado 
+# ano e em seguida informe se este ano é ou não bissexto.
+#------------------------------------------------------------------------------
+
+name = input()
+salary = float(input())
+sales_amount = float(input())
+final_salary = salary+((15/100)*sales_amount)
+print("TOTAL = R$ {0:.2f}".format(final_salary))
