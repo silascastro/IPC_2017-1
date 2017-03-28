55909f5ec1db04a8389d48edf8722a16390985e9
KethelenTamara <kettamara@gmail.com>
2017-03-28 13:47:08 -0300

    Update lista04_lista01_questao23.py

--- lista04/lista04_lista01_questao23.py
+++ lista04/lista04_lista01_questao23.py
@@ -1,3 +1,16 @@
+#------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr.
+#
+# João Vitor de Cordeiro B. Gonçalves   1515140036
+# Joelson Pereira Lima                  1715310060
+# Judá Salazar Braga                    1515200050
+# Kethelen Tamara Braga Barbosa         1525212002
+# Lucas Gabriel Silveira                1715310053
+
+
+
 num = float(input("Digite o numero à ser analisado: "))
 if round(num) == num:
 	print("O número digitado é inteiro")
