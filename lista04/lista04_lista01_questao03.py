c55eb94119d7578fa57377f29f5cb6b08a36adce
KethelenTamara <kettamara@gmail.com>
2017-03-28 13:44:45 -0300

    Update lista04_lista01_questao03.py

--- lista04/lista04_lista01_questao03.py
+++ lista04/lista04_lista01_questao03.py
@@ -1,3 +1,15 @@
+#------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr.
+#
+# João Vitor de Cordeiro B. Gonçalves   1515140036
+# Joelson Pereira Lima                  1715310060
+# Judá Salazar Braga                    1515200050
+# Kethelen Tamara Braga Barbosa         1525212002
+# Lucas Gabriel Silveira                1715310053
+
+
 print("Digite M para masculino e F para feminino")
 gender = input("Digite \"M\" ou \"F\": ")
 
