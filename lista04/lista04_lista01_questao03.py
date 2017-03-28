3f88fcf6d284d0f676408c80fa39612bfca9de26
joelson174 <joelson.jpl@gmail.com>
2017-03-28 00:29:46 -0400

    testando commits e upando questão

---
+++ lista04/lista04_lista01_questao03.py
@@ -0,0 +1,10 @@
+print("Digite M para masculino e F para feminino")
+gender = input("Digite \"M\" ou \"F\": ")
+
+if gender == "M":
+	print("O gênero digitado foi MASCULINO")
+else:
+	if gender == "F":
+		print("O gênero digitado foi FEMININO")
+	else:
+		print("O gênero digitado é INVÁLIDO")
