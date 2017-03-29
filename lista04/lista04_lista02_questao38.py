ab9bcd6001adfbb105ad33ab6b641b8d463d5387
tiagoweb <tiagoweb@gmail.com>
2017-03-29 18:54:06 -0400

    Update lista04_lista02_questao38.py

--- lista04/lista04_lista02_questao38.py
+++ lista04/lista04_lista02_questao38.py
@@ -7,7 +7,20 @@
 # Victor Hugo de Oliveira Carreira          1715310063
 #
 # URI Online Judge | 1038
-# Snack
+# Lanche
+#
+# Com base na tabela abaixo, escreva um programa que leia o código de um item 
+# e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+#
+# Entrada
+#
+# O arquivo de entrada contém dois valores inteiros correspondentes ao código 
+# e à quantidade de um item conforme tabela acima.
+#
+# Saída
+#
+# O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor 
+# a ser pago, com 2 casas após o ponto decimal.
 # --------------------------------------------------------------------------------------
 
 x = int(input())
