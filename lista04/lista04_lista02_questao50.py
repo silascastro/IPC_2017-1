63b2a760827af45817a3c2e8aaba57f043155b61
Wesley Rocha <wsr.snf17@uea.edu.br>
2017-03-29 18:55:42 -0400

    Update lista04_lista02_questao50.py

--- lista04/lista04_lista02_questao50.py
+++ lista04/lista04_lista02_questao50.py
@@ -21,22 +21,22 @@
 # o programa deverá informar: DDD nao cadastrado
 # ---------------------------------------------------------------------------------------------------------------------------------
 
-number= int(input("Insira o DDD: "))
-if number== 61:
+number = int(input("Insira o DDD: "))
+if number == 61:
     print("Brasilia")
-elif number== 71:
+elif number == 71:
     print("Salvador")
-elif number== 11:
+elif number == 11:
     print("São Paulo")
-elif number== 21:
+elif number == 21:
     print("Rio de Janeiro")
-elif number== 32:
+elif number == 32:
     print("Juiz de Fora")
-elif number== 19:
+elif number == 19:
     print("Campinas")
-elif number== 27:
+elif number == 27:
     print("Vitoria")
-elif number== 31:
+elif number == 31:
     print("Belo Horizonte")
 else:
     print("DDD not found")
