6bcbded6c4a4bcb98e5e0d315c558ba498b146bc
Wesley Rocha <wsr.snf17@uea.edu.br>
2017-03-29 18:50:31 -0400

    Update lista04_lista02_questao44.py

--- lista04/lista04_lista02_questao44.py
+++ lista04/lista04_lista02_questao44.py
@@ -2,6 +2,7 @@
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
+#
 # Guilherme Silva de Oliveira       1715310034
 # Gabriel Nascimento de Oliveira    1715310052
 # Gabriel de Queiroz Souza          1715310044
@@ -11,7 +12,7 @@
 # Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos",
 # indicando se os valores lidos são múltiplos entre si.
 # ----------------------------------------------------------------------------------------------------------------------
-a,b = raw_input().split()
+a, b = input().split()
 a = int(a)
 b = int(b)
 
