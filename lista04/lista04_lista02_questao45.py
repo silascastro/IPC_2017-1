b1804b0111bebb4fdfb7c0a0977cfaf0f7ce9bbb
erikatilio <erikatilio@users.noreply.github.com>
2017-03-29 18:51:24 -0400

    padrões

--- lista04/lista04_lista02_questao45.py
+++ lista04/lista04_lista02_questao45.py
@@ -25,14 +25,14 @@ A = float(A)
 B = float(B)
 C = float(C)
 
-if A >= B+C:
+if A >= B + C:
 	print("Não forma triangulo")
 else:
-    if (A**2) == (B**2)+(C**2):
+    if (A**2) == (B**2) + (C**2):
         print("Triangulo retângulo")
-    if (A**2) > (B**2)+(C**2):
+    if (A**2) > (B**2) + (C**2):
         print("Triangulo obtusangulo")
-    if (A**2) < (B**2)+(C**2):
+    if (A**2) < (B**2) + (C**2):
         print("Triangulo acutangulo")
 if A == B and A == C and B == C:
         print("Triangulo equilatero")
