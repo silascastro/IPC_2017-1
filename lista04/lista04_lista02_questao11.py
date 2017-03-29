c85601e91795ecf94ac87375f7baff478dccb1ba
Wesley Rocha <wsr.snf17@uea.edu.br>
2017-03-29 18:56:14 -0400

    Update lista04_lista02_questao11.py

--- lista04/lista04_lista02_questao11.py
+++ lista04/lista04_lista02_questao11.py
@@ -22,6 +22,6 @@
 #----------------------------------------------------------------------------------
 radius = float(input())
 pi = 3.14159
-volume = 4/3*pi*radius**3
+volume = 4/(3*pi*radius**3)
 
 print('volume = %.3f' % volume)
