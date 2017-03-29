61d9af1df919621328bc58161845445378293e3b
NayaraCerdeira <naycerdeira@gmail.com>
2017-03-29 18:39:01 -0400

    Update lista04_lista01_questao15.py

--- lista04/lista04_lista01_questao15.py
+++ lista04/lista04_lista01_questao15.py
@@ -11,16 +11,16 @@
 #Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo.
 # Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno
 #-------------------------------------------------------------------------------------
-l1=float(input("Digite o valor do lado"))
-l2=float(input("Digite outro valor do lado"))
-l3=float(input("Digite mais um valor do lado"))
-if l1+l2>l3:
+side1=float(input("Digite o valor do lado"))
+side2=float(input("Digite outro valor do lado"))
+side3=float(input("Digite mais um valor do lado"))
+if side1+side2>side3:
     print(" É possivel formar um triangulo ")
 else:
     print("nao é possivel formar um triangulo")
-if l1==l2==l3:
+if side1==side2==side3:
     print("Triangulo equilatero")
-if l1==l2!=l3 or l1==l3!=l2:
+if side1==side2!=side3 or side1==side3!=side2:
     print("Triangulo isosceles")
-if l1!=l2!=l3!=l1:
+if side1!=side2!=side3!=side1:
     print("Triangulo Escaleno")
