c81c99596c9ca06d04e4a2bb3edc93a03d5837d8
Wesley Rocha <wsr.snf17@uea.edu.br>
2017-03-29 18:53:19 -0400

    Update lista04_lista02_questao47.py

--- lista04/lista04_lista02_questao47.py
+++ lista04/lista04_lista02_questao47.py
@@ -3,21 +3,24 @@
 #Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
-#=======================================================================================
-#Reinaldo da Silva Vargas 1715310054
-#Natalia Cavalcante Xavier 1715310021
-#Nayara da Silva Cerdeira da Costa 1715310038
-#Marcus Vinicius Paes da Silva Santos 1515070060
-#Matheus de Oliveira Marques 1515310514
 #
-#faça um programa que peça as horas iniciais,minutos iniciais,horas finais e minutos finis de um jogo. em seguida imprima na tela
+# Reinaldo da Silva Vargas 1715310054
+# Natalia Cavalcante Xavier 1715310021
+# Nayara da Silva Cerdeira da Costa 1715310038
+# Marcus Vinicius Paes da Silva Santos 1515070060
+# Matheus de Oliveira Marques 1515310514
+#
+# Faça um programa que peça as horas iniciais,minutos iniciais,horas finais e minutos finis de um jogo. em seguida imprima na tela
 # a soma da partida do jogo
-hora_inicial = float(input('informe a hora inicial'))
-minuto_inicial = int(input('informe o minuto inicial'))
-hora_final= int(input('informe a hora final'))
-minuto_final= int(input('informe o minuto final'))
+#--------------------------------------------------------------------------
 
-calculando_time = (hora_inicial + hora_final)
-minutoscalc = (minuto_inicial + minuto_final)
+initial_hour =
+start_hour = float(input('informe a hora inicial'))
+start_minute = int(input('informe o minuto inicial'))
+end_hour = int(input('informe a hora final'))
+end_minute = int(input('informe o minuto final'))
 
-print('O jogo durou:', calculando_time, 'horas', 'e', minutoscalc, 'minutos')
+total_hour = (start_hour + end_hour)
+total_minute = (start_minute + end_minute)
+
+print('O jogo durou:', total_hour, 'horas', 'e', total_minute, 'minutos')
