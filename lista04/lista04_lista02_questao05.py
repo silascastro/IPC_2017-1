fb6cc6f607effdb6d92245419d649959eb5a1d13
LucaSilveira <lucas.gabrielsd@gmail.com>
2017-03-29 18:04:21 -0400

    Update lista04_lista02_questao05.py

--- lista04/lista04_lista02_questao05.py
+++ lista04/lista04_lista02_questao05.py
@@ -8,7 +8,7 @@
 # Judá Salazar Braga                    1515200050
 # Kethelen Tamara Braga Barbosa         1525212002
 # Lucas Gabriel Silveira                1715310053
-#
+# Silas Castro de Mendonça              1715310066
 #Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a
 #média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11).
 #Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
