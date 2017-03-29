11b6b977a3bd70dde2daf9e43b26ef09d2f97d8e
Gabriel Barroso <gabriel.barroso.am@gmail.com>
2017-03-29 17:08:45 -0400

    Grupo 3

---
+++ lista04/lista04_lista01_questao28.py
@@ -0,0 +1,64 @@
+#--------------------------------------------------------------------------
+
+# Universidade do Estado do Amazonas - UEA
+
+# Prof. Jucimar Jr
+
+#
+
+#Gabriel Barroso da Silva Lima 1715310011
+
+#Felipe Eduardo Silva de Almeida 1715310031
+
+#Frederico Victor Alfaia Rodrigues 1515200030
+
+#Diogo Duarte
+
+#Felipe Guerreiro de Mello 1315120052
+
+#Faça um Programa que peça dois números e imprima o maior deles. 
+#O Hipermercado Tabajara está com uma promoção de carnes que é imperdível. Confira:
+#
+#                          Até 5 Kg           Acima de 5 Kg
+#    File Duplo      R$ 4,90 por Kg          R$ 5,80 por Kg
+#    Alcatra         R$ 5,90 por Kg          R$ 6,80 por Kg
+#    Picanha         R$ 6,90 por Kg          R$ 7,80 por Kg
+
+#    Para atender a todos os clientes, cada cliente poderá levar apenas um dos tipos de carne da #promoção, porém não há limites para a quantidade de carne por cliente. Se compra for feita no #cartão Tabajara o cliente receberá ainda um desconto de 5% sobre o total a compra. Escreva um #programa que peça o tipo e a quantidade de carne comprada pelo usuário e gere um cupom fiscal, #contendo as informações da compra: tipo e quantidade de carne, preço total, tipo de pagamento, #valor do desconto e valor a pagar. 
+
+#-------------------------------------------------------------------------------------
+
+carne = int(input("1 - File Duplo | 2 - Alcatra | 3 - Picanha"))
+quant = int(input("Quantidade desejada(em quilos)"))
+tab = int(input("Tem o cartão Tabajara? (1 - sim | 0 - não)"))
+if quant <= 5:
+	if carne = 1:
+		preco = quant*4.9
+		tipo = "File Duplo"
+	else:
+		if carne = 2:
+			preco = quant*5.9
+			tipo = "Alcatra"
+		else: 
+			preco=quant*6.9
+			tipo = "Picanha"
+else:
+	if carne = 1:
+		preco = quant*5.8
+	else:
+		if carne = 2:
+			preco = quant*6.8
+		else:
+			preco = quant*7.8
+if tab = 1:
+	desco = preco*0.05
+	pag = "cartão"
+else:
+	desco = 0
+	pag = "dinheiro"
+print("Tipo da carne:", tipo)
+print("Quantidade:", quant, "Kg")
+print("Preco total:", preco)
+print("Tipo do pagamento:", pag)
+print("Valor do desconto:", desco)
+print("Valor a pagar:", preco-desco)
