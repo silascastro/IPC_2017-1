1acb8ac7bac39f72011a39a7a4d4e64efc028b21
LucaSilveira <lucas.gabrielsd@gmail.com>
2017-03-29 18:04:04 -0400

    Update lista04_lista02_questao45.py

--- lista04/lista04_lista02_questao45.py
+++ lista04/lista04_lista02_questao45.py
@@ -8,6 +8,7 @@
 # Judá Salazar Braga                    1515200050
 # Kethelen Tamara Braga Barbosa         1525212002
 # Lucas Gabriel Silveira                1715310053
+# Silas Castro de Mendonça              1715310066
 # Leia 3 valores de ponto flutuante A, B e C e ordene-os em ordem decrescente, de modo que o lado A representa o maior dos
 #3 lados. A seguir, determine o tipo de triângulo que estes três lados formam, com base nos seguintes casos, sempre escrevendo
 #uma mensagem adequada:
