63dde536ca74ad80c1745be01a83b7d5ea27afe4
LucaSilveira <lucas.gabrielsd@gmail.com>
2017-03-29 18:04:48 -0400

    Update lista04_lista02_questao35.py

--- lista04/lista04_lista02_questao35.py
+++ lista04/lista04_lista02_questao35.py
@@ -8,6 +8,7 @@
 # Judá Salazar Braga                    1515200050
 # Kethelen Tamara Braga Barbosa         1525212002
 # Lucas Gabriel Silveira                1715310053
+# Silas Castro de Mendonça              1715310066
 # Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, e a soma de C com D for
 #maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A for par escrever a mensagem "Valores aceitos",
 #senão escrever "Valores nao aceitos".
