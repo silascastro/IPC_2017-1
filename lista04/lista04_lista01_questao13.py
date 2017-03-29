364c39732b0eb338ec762e3c9b0dab2575d2eacb
Joelson Lima <joelson174@users.noreply.github.com>
2017-03-28 21:11:37 -0400

    Update lista04_lista01_questao13.py

--- lista04/lista04_lista01_questao13.py
+++ lista04/lista04_lista01_questao13.py
@@ -1,3 +1,16 @@
+#------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr.
+#
+# João Vitor de Cordeiro B. Gonçalves   1515140036
+# Joelson Pereira Lima                  1715310060
+# Judá Salazar Braga                    1515200050
+# Kethelen Tamara Braga Barbosa         1525212002
+# Lucas Gabriel Silveira                1715310053
+#Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro
+#valor deve aparecer valor inválido. .
+
 dia = int(input("Digite um numero de 1 a 7: "))
 if dia == 1 or dia == 2 or dia == 3 or dia == 4 or dia == 5 or dia == 6 or dia == 7:
 	if dia == 1:
