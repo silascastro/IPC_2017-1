9ff03b28f167bb2b2cc70e915b293b24d173f3e8
Gabriel Queiroz <gabriel.dequeirozsousa@gmail.com>
2017-03-29 15:26:52 -0400

    Grupo04

---
+++ lista04/lista04_lista02_questao44.py
@@ -0,0 +1,24 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Guilherme Silva de Oliveira       1715310034
+# Gabriel Nascimento de Oliveira    1715310052
+# Gabriel de Queiroz Souza          1715310044
+# Hugo Thadeu Silva Cardoso         1715310013
+# Ian Gabriel Costa Machado         1215120276
+#
+# Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos",
+# indicando se os valores lidos são múltiplos entre si.
+# ----------------------------------------------------------------------------------------------------------------------
+a,b = raw_input().split()
+a = int(a)
+b = int(b)
+
+num1 = a % b
+num2 = b % a
+
+if num1 == 0 or num2 == 0:
+    print ("Sao Multiplos")
+else:
+    print ("Nao sao Multiplos")
