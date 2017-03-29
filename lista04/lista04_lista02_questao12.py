0b959a566e89714fe703af8a801c93515e362e57
adhamlucas <adhamlucas14@hotmail.com>
2017-03-29 18:41:37 -0400

    Update lista04_lista02_questao12.py

--- lista04/lista04_lista02_questao12.py
+++ lista04/lista04_lista02_questao12.py
@@ -27,14 +27,14 @@ value = input().split()
 a, b, c = value
 pi = 3.14159
 
-triangle = (float(a)*float(c))/2
-circle = pi*(float(c)*float(c))
-trapezius = float(c)*(float(a)+float(b)) / 2
-square = float(b)*float(b)
-rectangle = float(a)*float(b)
+triangle = (float(a) * float(c)) / 2
+circle = pi*(float(c) * float(c))
+trapezius = float(c) * (float(a) + float(b)) / 2
+square = float(b) * float(b)
+rectangle = float(a) * float(b)
 
-print("TRIANGULO: %0.3f"%triangle)
-print("CIRCULO: %0.3f"%circle)
-print("TRAPEZIO: %0.3f"%trapezius)
-print("QUADRADO: %0.3f"%square)
-print("RETANGULO: %0.3f"%rectangle)
+print("TRIANGULO: %0.3f" % triangle)
+print("CIRCULO: %0.3f" % circle)
+print("TRAPEZIO: %0.3f" % trapezius)
+print("QUADRADO: %0.3f" % square)
+print("RETANGULO: %0.3f" % rectangle)
