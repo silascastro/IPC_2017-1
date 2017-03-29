d6ec70543b03b1388037c63c059e307590e304e5
Joelson Lima <joelson174@users.noreply.github.com>
2017-03-29 09:45:03 -0400

    Update lista04_lista02_questao45.py

--- lista04/lista04_lista02_questao45.py
+++ lista04/lista04_lista02_questao45.py
@@ -1,3 +1,23 @@
+#------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr.
+#
+# João Vitor de Cordeiro B. Gonçalves   1515140036
+# Joelson Pereira Lima                  1715310060
+# Judá Salazar Braga                    1515200050
+# Kethelen Tamara Braga Barbosa         1525212002
+# Lucas Gabriel Silveira                1715310053
+# Leia 3 valores de ponto flutuante A, B e C e ordene-os em ordem decrescente, de modo que o lado A representa o maior dos
+#3 lados. A seguir, determine o tipo de triângulo que estes três lados formam, com base nos seguintes casos, sempre escrevendo
+#uma mensagem adequada:
+	#se A ≥ B+C, apresente a mensagem: NAO FORMA TRIANGULO
+	#se A2 = B2 + C2, apresente a mensagem: TRIANGULO RETANGULO
+	#se A2 > B2 + C2, apresente a mensagem: TRIANGULO OBTUSANGULO
+	#se A2 < B2 + C2, apresente a mensagem: TRIANGULO ACUTANGULO
+	#se os três lados forem iguais, apresente a mensagem: TRIANGULO EQUILATERO
+	#se apenas dois dos lados forem iguais, apresente a mensagem: TRIANGULO ISOSCELES
+
 nums = (input().split())
 A,B,C = nums
 A = float(A)
