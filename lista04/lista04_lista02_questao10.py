d3096b6eb5f3f954253e6adabebddd5d9ff220a0
Vitor Summer <vitor.summer@hotmail.com>
2017-03-29 18:39:07 -0400

    Update lista04_lista02_questao10.py

--- lista04/lista04_lista02_questao10.py
+++ lista04/lista04_lista02_questao10.py
@@ -26,4 +26,4 @@ cod2, qtde2, valor2 = linha2
 
 total = (int(qtde1) * float(valor1)) + (int(qtde2) * float(valor2))
 
-print("VALOR A PAGAR: R$ %0.2f" %total)
+print("VAlor a pagar: R$ %0.2f" %total)
