f741c0433924cb2909116e603016bdb3e1aaa281
Aracille <cily.barbosa@gmail.com>
2017-03-29 16:06:22 -0400

    Create lista04_lista02_questao50.py
    Questão 50 (1050 lista02)

---
+++ lista04/lista04_lista02_questao50.py
@@ -0,0 +1,26 @@
+# ---------------------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+#Prof. Jucimar Jr
+# Aracille de souza Barbosa - 1315120206
+#
+# ---------------------------------------------------------------------------------------------------------------------------------
+number= int(input("Insira o DDD: "))
+if number== 61:
+    print("Brasilia")
+elif number== 71:
+    print("Salvador")
+elif number== 11:
+    print("São Paulo")
+elif number== 21:
+    print("Rio de Janeiro")
+elif number== 32:
+    print("Juiz de Fora")
+elif number== 19:
+    print("Campinas")
+elif number== 27:
+    print("Vitoria")
+elif number== 31:
+    print("Belo Horizonte")
+else:
+    print("DDD not found")
