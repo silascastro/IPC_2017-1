aab85b5d535b5bdba369f7cd1f11a79b5f64d2f3
VictorHuguw <kibedasarabia@gmail.com>
2017-03-29 19:01:33 -0400

    Update lista04_lista02_questao38.py

--- lista04/lista04_lista02_questao38.py
+++ lista04/lista04_lista02_questao38.py
@@ -11,8 +11,15 @@
 #
 # Com base na tabela abaixo, escreva um programa que leia o código de um item 
 # e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
-#
-# Entrada
+# 
+#  Codigo           especificaçao        preço
+#    1             Cachorro Quente      R$ 4,00
+#    2                X-Salada          R$ 4,50
+#    3                X-Bacon           R$ 5,00
+#    4             Torrada Simples      R$ 2,00
+#    5              Refrigerante        R$ 1,50
+# 
+#Entrada
 #
 # O arquivo de entrada contém dois valores inteiros correspondentes ao código 
 # e à quantidade de um item conforme tabela acima.
