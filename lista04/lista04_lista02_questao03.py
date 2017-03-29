58e0a7b2decae0adf99c725aecb5011e5ccebdb2
Gabriel Barroso <gabriel.barroso.am@gmail.com>
2017-03-29 13:07:16 -0400

    Grupo 3

---
+++ lista04/lista04_lista02_questao03.py
@@ -0,0 +1,18 @@
+#--------------------------------------------------------------------------
+#Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+#Gabriel Barroso da Silva Lima 1715310011
+#Felipe Eduardo Silva de Almeida 1715310031
+#Frederico Victor Alfaia Rodrigues 
+1515200030
+#Diogo Duarte
+#Felipe Guerreiro de Mello 1315120052
+#Criar um programa que receba dois valores e mostre a sua soma
+#-------------------------------------------------------------------------------------
+
+a = int(input("digite um valor: "))
+b = int(input("digite outro valor: "))
+SOMA = a+b
+print ("SOMA =", SOMA)
