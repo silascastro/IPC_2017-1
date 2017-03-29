d995c7c8e0d6282c14b7affaa2e687beea033203
Felipe Almeida <edu468felipe@gmail.com>
2017-03-29 14:28:09 -0400

    Grupo 3
    Colocando em ordem de chamada

--- lista04/lista04_lista02_questao43.py
+++ lista04/lista04_lista02_questao43.py
@@ -1,16 +1,16 @@
 #--------------------------------------------------------------------------
-#Introdução a Programação de Computadores - IPC
+#IntroduÃ§Ã£o a ProgramaÃ§Ã£o de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
 #
-#Gabriel Barroso da Silva Lima 1715310011
-#Felipe Eduardo Silva de Almeida 1715310031
-#Frederico Victor Alfaia Rodrigues 
-1515200030
-#Diogo Duarte
-#Felipe Guerreiro de Mello 1315120052
-#Criar um programa que receba três medidas e indique o perímetro do triângulo.
-#Se não for possível, calcular o trapézio tendo a e b como bases e c como altura
+# Diogo Duarte
+# Felipe Eduardo Silva de Almeida   1715310031
+# Felipe Guerreiro de Mello         1315120052
+# Frederico Victor Alfaia Rodrigues 1515200030
+# Gabriel Barroso da Silva Lima     1715310011
+#
+# Criar um programa que receba trÃªs medidas e indique o perÃ­metro do triÃ¢ngulo.
+# Se nÃ£o for possÃ­vel, calcular o trapÃ©zio tendo a e b como bases e c como altura
 #-------------------------------------------------------------------------------------
 
 a = float(input("digite uma medida"))
