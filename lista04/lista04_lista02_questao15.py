df1f60ae93818d57d9333301f1f1fdec947bd3b1
LucaSilveira <lucas.gabrielsd@gmail.com>
2017-03-29 18:04:31 -0400

    Update lista04_lista02_questao15.py

--- lista04/lista04_lista02_questao15.py
+++ lista04/lista04_lista02_questao15.py
@@ -9,6 +9,7 @@
 # Judá Salazar Braga                    1515200050
 # Kethelen Tamara Braga Barbosa         1525212002
 # Lucas Gabriel Silveira                1715310053
+# Silas Castro de Mendonça              1715310066
 # Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
 
 line1 = (input().split())
