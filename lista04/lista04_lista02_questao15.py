221c5c0280b5d9bd452f2dfe78c5b8d61fc31fae
Vitor Summer <vitor.summer@hotmail.com>
2017-03-29 18:41:16 -0400

    Update lista04_lista02_questao15.py

--- lista04/lista04_lista02_questao15.py
+++ lista04/lista04_lista02_questao15.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #------------------------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
@@ -11,6 +10,7 @@
 # Lucas Gabriel Silveira                1715310053
 # Silas Castro de Mendonça              1715310066
 # Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+#------------------------------------------------------------------------------
 
 line1 = (input().split())
 line2 = (input().split())
