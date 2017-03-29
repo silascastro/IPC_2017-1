650911bcc755ea5d8a49e660e9d7b9022cf36896
cetdo <cetdo@outlook.com>
2017-03-29 14:44:02 -0400

    upando questão

---
+++ lista04/lista04_lista02_questao41.py
@@ -0,0 +1,44 @@
+#----------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+#
+# Adham Lucas da Silva Oliveira         1715310059
+# Alexandre Marques Uchôa               1715310028
+# André Luís Laborda Neves              1515070006
+# Carlos Eduardo Tapudima de Oliveira	1715310030
+# Diego Reis Figueira                   1515070169
+#
+# Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+# de um ponto em um plano. A seguir,
+# determine qual o quadrante ao qual pertence o ponto, ou se está
+# sobre um dos eixos cartesianos ou na origem (x = y = 0).
+# Se o ponto estiver na origem, escreva a mensagem “Origem”.
+# Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+
+
+
+x , y = input().split(' ')
+x = float(x)
+y = float(y)
+
+
+if x == y == 0:
+    print('Origem')
+
+elif x > 0 < y:
+    print('Q1')
+
+elif x < 0 < y:
+    print('Q2')
+
+elif x < 0 > y:
+    print('Q3')
+
+elif x > 0 > y:
+    print('Q4')
+
+elif x == 0 != y:
+    print('Eixo Y')
+
+else:
+    print('Eixo X')
