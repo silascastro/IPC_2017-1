58298799e2e1edec2330910b30515fd78c7e7aa3
GnascimentoSI <nascimentoUEA@hotmail.com>
2017-03-29 18:26:37 -0400

    Update lista04_lista01_questao04.py

--- lista04/lista04_lista01_questao04.py
+++ lista04/lista04_lista01_questao04.py
@@ -6,9 +6,9 @@
 #
 # Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 
-l = input('write a letter: ')
+letra = input('write a letter: ')
 
-if l == 'a' or l == 'e' or l == 'i' or l == 'o' or l == 'u':
+if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
     print('the letter is a vowel')
 else:
     print('the letter is a consonant')
