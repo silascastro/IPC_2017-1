73bcaa7a77e997d5cc73740d302520ca0b3c0ffa
Felipe Almeida <edu468felipe@gmail.com>
2017-03-29 14:42:33 -0400

    Grupo 3
    Organizando o cabeçalho

--- lista04/lista04_lista01_questao21.py
+++ lista04/lista04_lista01_questao21.py
@@ -1,22 +1,22 @@
 #--------------------------------------------------------------------------
-#IntroduÁ„o a ProgramaÁ„o de Computadores - IPC
+#Introdu√ß√£o a Programa√ß√£o de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
 #
-#Gabriel Barroso da Silva Lima 1715310011
-#Felipe Eduardo Silva de Almeida 1715310031
-#Frederico Victor Alfaia Rodrigues 
-1515200030
-#Diogo Duarte
-#Felipe Guerreiro de Mello 1315120052
-#FaÁa um Programa para um caixa eletrÙnico. O programa dever· perguntar ao usu·rio a valor do saque e depois informar 
-#quantas notas de cada valor ser„o fornecidas. As notas disponÌveis ser„o as de 1, 5, 10, 50 e 100 reais. O valor mÌnimo 
-#È de 10 reais e o m·ximo de 600 reais. O programa n„o deve se preocupar com a quantidade de notas existentes na m·quina. 
+# Diogo Duarte
+# Felipe Eduardo Silva de Almeida   1715310031
+# Felipe Guerreiro de Mello         1315120052
+# Frederico Victor Alfaia Rodrigues 1515200030
+# Gabriel Barroso da Silva Lima     1715310011
+#
+#Fa√ßa um Programa para um caixa eletr√¥nico. O programa dever√° perguntar ao usu√°rio a valor do saque e depois informar 
+#quantas notas de cada valor ser√£o fornecidas. As notas dispon√≠veis ser√£o as de 1, 5, 10, 50 e 100 reais. O valor m√≠nimo 
+#√© de 10 reais e o m√°ximo de 600 reais. O programa n√£o deve se preocupar com a quantidade de notas existentes na m√°quina. 
 #-------------------------------------------------------------------------------------
 
 saq = float(input("valor do saque: "))
 while saq < 10 or saq > 600:
-    saq = float(input("valor inv·lido, digite um valor entre R$10 e R$600: "))
+    saq = float(input("valor inv√°lido, digite um valor entre R$10 e R$600: "))
 n100 = int(saq / 100)
 saq = saq - (n100 * 100)
 n50 = saq / 50
@@ -25,7 +25,7 @@ n10 = saq / 10
 saq = saq - (n10 * 10)
 n5 = saq / 5
 saq = saq - (n5 * 5)
-print ("ser„o necess·rias: ")
+print ("ser√£o necess√°rias: ")
 print (n100, "nota(s) de R$100")
 print (n50, "nota(s) de R$50")
 print (n10, "nota(s) de R$10")
