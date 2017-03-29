e1a529935c7b173459a32352a43cbb8aba527ae4
evandro7047301 <evandro7047301@gmail.com>
2017-03-29 18:42:15 -0400

    Update lista04_lista02_questao22.py

--- lista04/lista04_lista02_questao22.py
+++ lista04/lista04_lista02_questao22.py
@@ -23,7 +23,7 @@ B = float(b)
 C = float(c)
 D = float(d)
 
-if(B > C and D > C and C+D > A+B and C > 0 or D > 0 and (A/2) == 0):
+if(B > C and D > C and C+D > A + B and C > 0 or D > 0 and (A / 2) == 0):
     print('Valores aceitos')
 else:
     print('Valores nao aceitos')
