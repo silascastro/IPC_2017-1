b3cc74e4497fac7cc7273536e23d97d242583e73
Vitor Summer <vitor.summer@hotmail.com>
2017-03-29 18:50:44 -0400

    Update lista04_lista02_questao50.py

--- lista04/lista04_lista02_questao50.py
+++ lista04/lista04_lista02_questao50.py
@@ -1,10 +1,26 @@
 # ---------------------------------------------------------------------------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
-#Prof. Jucimar Jr
-# Aracille de souza Barbosa - 1315120206
+# Prof. Jucimar Jr
+# Aracille de souza Barbosa   1315120206
 #
+# Leia um número inteiro que representa um código de DDD para discagem interurbana.
+# Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
+#
+#     DDD    DESTINATION
+#     61     Brasília
+#     71     São Paulo
+#     11     Salvador
+#     21     Rio de Janeiro
+#     32     Juiz de Fora
+#     19     Campinas
+#     27     Vitoria
+#     31     Belo Horizonte
+# 
+# Se a entrada for qualquer outro DDD que não esteja presente na tabela acima,
+# o programa deverá informar: DDD nao cadastrado
 # ---------------------------------------------------------------------------------------------------------------------------------
+
 number= int(input("Insira o DDD: "))
 if number== 61:
     print("Brasilia")
