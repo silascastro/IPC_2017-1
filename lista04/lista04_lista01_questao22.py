6b6c6f37f81dd0f2e1f1332f4762f3c7e02250a9
Gabriel Queiroz <gabriel.dequeirozsousa@gmail.com>
2017-03-29 16:45:57 -0400

    Grupo04

--- lista04/lista04_lista01_questao22.py
+++ lista04/lista04_lista01_questao22.py
@@ -1,3 +1,15 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Guilherme Silva de Oliveira       1715310034
+# Gabriel Nascimento de Oliveira    1715310052
+# Gabriel de Queiroz Souza          1715310044
+# Hugo Thadeu Silva Cardoso         1715310013
+# Ian Gabriel Costa Machado         1215120276
+# Faça um Programa que peça um número inteiro e determine se ele é par ou impar.
+# Dica: utilize o operador módulo (resto da divisão). 
+# ----------------------------------------------------------------------------------------------------------------------
 num = int(input("Informe um valor"))
 
 if(num % 2 == 0):
