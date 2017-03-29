360000d81605e3c2c79083a294fa055f1db740c5
reinaldo22 <vargas.reynaldo22@gmail.com>
2017-03-28 23:55:10 -0400

    Update lista04_list02_exercicio47.py

--- lista04/lista04_list02_exercicio47.py
+++ lista04/lista04_list02_exercicio47.py
@@ -1,3 +1,17 @@
+
+#--------------------------------------------------------------------------
+#Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#=======================================================================================
+#Reinaldo da Silva Vargas 1715310054
+#Natalia Cavalcante Xavier 1715310021
+#Nayara da Silva Cerdeira da Costa 1715310038
+#Marcus Vinicius Paes da Silva Santos 1515070060
+#Matheus de Oliveira Marques 1515310514
+#
+#faça um programa que peça as horas iniciais,minutos iniciais,horas finais e minutos finis de um jogo. em seguida imprima na tela
+# a soma da partida do jogo
 hora_inicial = float(input('informe a hora inicial'))
 minuto_inicial = int(input('informe o minuto inicial'))
 hora_final= int(input('informe a hora final'))
