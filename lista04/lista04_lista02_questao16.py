10e6a3d484bf6c2d5ff255551db36d20b02f2f3d
natcavalcantex21 <nataliacxavier1@gmail.com>
2017-03-29 17:21:55 -0400

    Add files via upload

---
+++ lista04/lista04_lista02_questao16.py
@@ -0,0 +1,14 @@
+#--------------------------------------------------------------------------
+#Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+
+#Reinaldo da Silva Varas 1715310054
+#Natalia Cavalcante Xavier 1715310021
+#Nayara da Silva Cerdeira da Costa 1715310038
+#Marcus Vinicius Paes da Silva Santos 1515070060
+#Matheus de Oliveira Marques 1515310514
+#--------------------------------------------------------
+km = int(input("Digite a distancia "))
+time = km*2
+print(time,"minutos")
