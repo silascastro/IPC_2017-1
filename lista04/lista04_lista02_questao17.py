30edf9468170dd847812ff1e55773f59e6760036
natcavalcantex21 <nataliacxavier1@gmail.com>
2017-03-29 18:51:45 -0400

    Update lista04_lista02_questao17.py

--- lista04/lista04_lista02_questao17.py
+++ lista04/lista04_lista02_questao17.py
@@ -8,7 +8,12 @@
 #Nayara da Silva Cerdeira da Costa 1715310038
 #Marcus Vinicius Paes da Silva Santos 1515070060
 #Matheus de Oliveira Marques 1515310514
-#Criar um programa que indique quantos litros sao necessários para realizar uma viagem
+#Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, 
+#ao utilizar um automóvel que faz 12 KM/L.
+#Para isso, ele gostaria que você o auxiliasse através de um simples programa.
+#Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h)
+#Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários.
+#Mostre o valor com 3 casas decimais após o pont
 #-------------------------------------------------------------------------------------
 
 timetrip = float(input("Informe o tempo de viagem: "))
