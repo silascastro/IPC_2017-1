343898e2e8e8c6a6e4ff5bc67b46a23efc7875f3
Aracille <cily.barbosa@gmail.com>
2017-03-29 17:28:15 -0400

    Update lista04_lista02_questao40.py
    questão 40 da lista02

--- lista04/lista04_lista02_questao40.py
+++ lista04/lista04_lista02_questao40.py
@@ -1,59 +1,31 @@
-#-----------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
-# Prof. Jucimar Jr.
-# Erik Atlio Silva Rey                  1715310059
-# Edson de Lima Barros                  1715310043
-# Enrique Leão Barbosa Izel             1715310048
-# Evandro Padilha Barroso Filho         1715310009
-# Fang Yao                              1115180236
+#Prof. Jucimar Jr
+# Aracille de souza Barbosa - 1315120206
 #
-#Leia quatro números (N1, N2, N3, N4), cada um deles com uma casa decimal, correspondente às quatro notas de um aluno
-#Calcule a média com pesos 2, 3, 4 e 1, respectivamente, para cada uma destas notas e mostre esta média acompanhada 
-#pela mensagem "Media: ". Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.".
-#Se a média calculada for inferior a 5.0, imprima a mensagem "Aluno reprovado.".
-#Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, o programa deve imprimir a mensagem "Aluno em exame.".
-#No caso do aluno estar em exame, leia um valor correspondente à nota do exame obtida pelo aluno.
-#Imprima então a mensagem "Nota do exame: " acompanhada pela nota digitada. 
-#Recalcule a média (some a pontuação do exame com a média anteriormente calculada e divida por 2).
-#e imprima a mensagem "Aluno aprovado." (caso a média final seja 5.0 ou mais ) ou "Aluno reprovado.",
-#(caso a média tenha ficado 4.9 ou menos). Para estes dois casos (aprovado ou reprovado após ter pego exame)
-#apresente na última linha uma mensagem "Media final: " seguido da média final para esse aluno.
-#----------------------------------------------------------------------------------------------------------------------
-
-n1, n2, n3, n4 = input().split()
-
-n1 = float(n1)
-n2 = float(n2)
-n3 = float(n3)
-n4 = float(n4)
-
-n1 = n1 * 2
-n2 = n2 * 3
-n3 = n3 * 4
-n4 = n4 * 1
-
-Media = (n1 + n2 + n3 + n4)/10
-if Media >= 5.0 and Media <= 6.9:
-    n5 = float(input())
-print("Media: %.1f" %Media)
-
-if Media >= 7.0:
-    print("Aluno aprovado.")
-
-if Media < 5.0:
-        print("Aluno reprovado.")
-
-if Media >= 5.0 and Media <= 6.9:
-    print("Aluno em exame.")
-
-
-    print("Nota do exame:",n5)
-    Media2 = (n5 + Media)/2
-
-    if Media2 >= 5.0:
-        print("Aluno aprovado.")
-        print("Media final: %.1f" %Media2)
+# --------------------------------------------------------------------------------------------------------------------
+n1= float(input("Informe a 1º nota: "))
+n2= float(input("Informe a 2º nota: "))
+n3= float(input("Informe a 3º nota: "))
+n4= float(input("Informe a 4º nota: "))
+n1p=n1*2
+n2p=n2*3
+n3p=n3*4
+n4p=n4*1
+media= ((n1p+n2p+n3p+n4p)/4)
+print("Média: %.1f" %media)
+if media>= 7.0:
+    print("Aluno aprovado")
+elif media< 5.0:
+    print("Aluno reprovado")
+elif 5<=media<=6.9:
+    print("Aluno em exame")
+if 5<=media<=6.9:
+    exame=float(input("Nota do exame:"))
+    media2=((media+exame)/2)
+    print("Média do exame: %.1f" %media2)
+    if media2 >=5:
+        print("aprovado!")
     else:
-        print("Aluno reprovado.")
-        print("Media final: %.1f" %Media2)
+        print("Reprovado no Exame")
