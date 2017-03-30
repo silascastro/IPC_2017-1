ee93a175414db3b75f3ffc0fb1300e63d12fb814
erikatilio <erikatilio@users.noreply.github.com>
2017-03-30 11:44:33 -0400

    Padrão

--- lista04/lista04_lista02_questao44.py
+++ lista04/lista04_lista02_questao44.py
@@ -12,6 +12,7 @@
 # Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos",
 # indicando se os valores lidos são múltiplos entre si.
 # ----------------------------------------------------------------------------------------------------------------------
+
 A, B = input().split()
 A = int(A)
 B = int(B)
