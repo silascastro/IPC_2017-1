96eb256e05626ba333d9d9d19ca1bf55ae631432
Evandro Padilha Barroso FIlho <evandro7047301@gmail.com>
2017-04-06 09:27:03 -0400

    cabeçalho incompleto, aguardando nomes, matriculas

--- lista05/lista05_lista01_questao03.py
+++ lista05/lista05_lista01_questao03.py
@@ -1,9 +1,19 @@
-#03.Faça um programa que leia e valide as seguintes informações:
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# nome completo do aluno 1	matrícula do aluno 1
+# nome completo do aluno 2	matrícula do aluno 2
+# nome completo do aluno 3	matrícula do aluno 3
+# nome completo do aluno 4	matrícula do aluno 4
+#
+# #03.Faça um programa que leia e valide as seguintes informações:
 #Nome: maior que 3 caracteres;
 #Idade: entre 0 e 150;
 #Salário: maior que zero;
 #Sexo: 'f' ou 'm';
 #Estado Civil: 's', 'c', 'v', 'd';
+# ----------------------------------------------------------
 
 name = (input("Digite seu nome (min: 3 caracteres): "))
 age = int(input("Digite sua idade: "))
