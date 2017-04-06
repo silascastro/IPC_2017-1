01b4cfe713b36b58d81596c3fd29cd29278a8632
Evandro Padilha Barroso FIlho <evandro7047301@gmail.com>
2017-04-06 17:00:08 -0400

    ajustando lógica

--- lista05/lista05_lista01_questao03.py
+++ lista05/lista05_lista01_questao03.py
@@ -15,25 +15,28 @@
 #Estado Civil: 's', 'c', 'v', 'd';
 # ----------------------------------------------------------
 
-name = (input("Digite seu nome (min: 3 caracteres): "))
-age = int(input("Digite sua idade: "))
-sallary = float(input("Digite seu salário: "))
-sex = input("Digite seu sexo usando f para feminino e m para masculino: ")
-civil_state = input("Digite seu estado civil(s, c, v, d): ")
-
-def validation(name, age, sallary, sex, civil_state,):
-    if len(name) < 3 or age < 0 or age > 150 or sallary <= 0 or sex not in ['f', 'm'] or civil_state not in ['s', 'c',
-                                                                                                            'v','d']:
-        return False
-    else:
-        validation == True
-
-while validation(name, age,sallary, sex, civil_state) == False:
-    print("Algum campo está invalido, por favor digite novamente os campos")
+name = input("Digite seu nome (min: 3 caracteres): ")
+while len(name) < 3:
+    print("Campo invalido :")
     name = input("Digite seu nome (min: 3 caracteres): ")
+
+age = int(input("Digite sua idade: "))
+while age < 0 or age > 150:
+    print("Campo invalido :")
     age = int(input("Digite sua idade: "))
+
+sallary = float(input("Digite seu salário: "))
+while sallary <= 0:
+    print("Campo invalido :")
     sallary = float(input("Digite seu salário: "))
+
+sex = input("Digite seu sexo usando f para feminino e m para masculino: ")
+while sex != 'f' and sex != 'm':
+    print("Campo invalido :")
     sex = input("Digite seu sexo usando f para feminino e m para masculino: ")
+
+civil_state = input("Digite seu estado civil(s, c, v, d): ")
+while civil_state != 's' and civil_state != 'c' and civil_state != 'v' and civil_state != 'd':
     civil_state = input("Digite seu estado civil(s, c, v, d): ")
 
-print("Informações atualizadas")
+print("Informações atualizadas") 
