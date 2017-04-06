7aa6508774f900aba394dc8640286d3faedf7775
Evandro Padilha Barroso FIlho <evandro7047301@gmail.com>
2017-04-06 09:34:13 -0400

    ajustando cabeçalho

--- lista05/lista05_lista01_questao03.py
+++ lista05/lista05_lista01_questao03.py
@@ -2,7 +2,7 @@
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
-# nome completo do aluno 1	matrícula do aluno 1
+# Evandro Padilha Barroso Filho 1715310009 
 # nome completo do aluno 2	matrícula do aluno 2
 # nome completo do aluno 3	matrícula do aluno 3
 # nome completo do aluno 4	matrícula do aluno 4
