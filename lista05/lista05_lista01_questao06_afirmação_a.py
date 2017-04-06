fe7c6d71dc7e0f8d0223be06fc6c6d0cd64a4dfc
LukasMS <lukasmichel66@gmail.com>
2017-04-06 19:42:25 -0400

    Update lista05_lista01_questao06_afirmação_a.py

--- lista05/lista05_lista01_questao06_afirmação_a.py
+++ lista05/lista05_lista01_questao06_afirmação_a.py
@@ -4,7 +4,11 @@
 #
 # Prof. Jucimar Jr 
 #
-# Lukas Michel Souza Mota 1715310018
+# Lukas Michel Souza Mota               1715310018
+# Marcus Viniciius Paes da Silva Santos 1515070060
+# Matheus de Oliveira Marques           1515310514
+# Natália Cavalcante Xavier             1715310021
+# Nayara da Silva Cerdeira da Costa     1715310038
 #
 # Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro.
 # Depois modifique o programa para que ele mostre os números um ao lado do outro. 
