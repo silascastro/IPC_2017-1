63ede1a1b6db1ccdf1ea137d32af0184f7f2aa2e
Iury S.C <coelhosc13@gmail.com>
2017-04-07 18:31:37 -0400

    Update lista05_lista01_questao01.py

--- lista05/lista05_lista01_questao01.py
+++ lista05/lista05_lista01_questao01.py
@@ -5,7 +5,7 @@
 # Adham Lucas da Silva Oliveira           1715310059
 # Alexandre Marques Uchôa                 1715310028
 # André Luís Laborda Neves                1515070006
-# Carlos Eduardo Tapudima de Oliveira	    1715310030
+# Carlos Eduardo Tapudima de Oliveira	   1715310030
 # Aracille de Souza Barbosa               1315120206
 # Dayana Picanço Marquez                  1715310058
 #
