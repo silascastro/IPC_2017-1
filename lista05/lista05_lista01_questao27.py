29ed08a7cdb193275bb47c090300593761bd5030
Evandro Padilha Barroso Filho <evandro7047301@gmail.com>
2017-04-07 19:27:10 -0400

    Update lista05_lista01_questao27.py

--- lista05/lista05_lista01_questao27.py
+++ lista05/lista05_lista01_questao27.py
@@ -1,3 +1,18 @@
+#----------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Evandro Padilha Barroso Filho     1715310009 
+# Gabriel Barroso da Silva Lima     1715310011
+# Frederico Victor Alfaia Rodrigues 1515200030 
+# Felipe Eduardo Silva de Almeida   1715310031 
+# Felipe Guerreiro De Mello         1315120052 
+# Silas castro de Mendonça          1715310066
+#
+#Faça um programa que calcule o número médio de alunos por turma. 
+#Para isto, peça a quantidade de turmas e a quantidade de alunos
+# para cada #turma. As turmas não podem ter mais de 40 alunos. 
+#----------------------------------------------------------------
 classes = int(input("Informe o número de turmas "))
 
 c = 0
