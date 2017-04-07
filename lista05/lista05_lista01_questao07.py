8d0df14b5717012016760103234d01f1c41ce369
VictorHuguw <kibedasarabia@gmail.com>
2017-04-06 21:37:54 -0400

    Update lista05_lista01_questao07.py

--- lista05/lista05_lista01_questao07.py
+++ lista05/lista05_lista01_questao07.py
@@ -1,6 +1,6 @@
 number1 = int(input("digite um numero: "))
 number2 = int(input("digite mais um numero: "))
-number3 = int(input("digite mais um numero"))
+number3 = int(input("digite mais um numero: "))
 number4 = int(input("digite mais um numero"))
 number5 = int(input("digite mais um numero"))
 
