ba29813ecd2ab293e1b8f4cc2180e9d4e106902b
VictorHuguw <kibedasarabia@gmail.com>
2017-04-06 21:30:51 -0400

    Update lista05_lista01_questao07.py

--- lista05/lista05_lista01_questao07.py
+++ lista05/lista05_lista01_questao07.py
@@ -3,3 +3,19 @@ numb2 = int(input("digite mais um numero"))
 numb3 = int(input("digite mais um numero"))
 numb4 = int(input("digite mais um numero"))
 numb5 = int(input("digite mais um numero"))
+
+while numb1 > numb2 and numb1 > numb3 and numb1 > numb4 and numb1 > numb5:
+    print("o mair numero e o", numb1)
+    break
+while numb2 > numb1 and numb2 > numb3 and numb2 > numb4 and numb2 > numb5:
+    print("o mair numero e o", numb2)
+    break
+while numb3 > numb1 and numb3 > numb2 and numb3 > numb4 and numb3 > numb5:
+    print("o mair numero e o", numb3)
+    break
+while numb4 > numb1 and numb4 > numb2 and numb4 > numb3 and numb4 > numb5:
+    print("o mair numero e o", numb4)
+    break
+while numb5 > numb1 and numb5 > numb2 and numb5 > numb3 and numb5 > numb4:
+    print("o mair numero e o", numb5)
+    break
