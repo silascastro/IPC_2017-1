5302782e4f91a90ac00274d7fff1f1be6b85b748
VictorHuguw <kibedasarabia@gmail.com>
2017-04-07 10:56:51 -0300

    Update lista05_lista02_questão07.py

--- lista05/lista05_lista02_questão07.py
+++ lista05/lista05_lista02_questão07.py
@@ -2,12 +2,15 @@
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
-
-
-
-
+# ULISSES ANTONIO ANTONINO DA COSTA  1515090555
+# TIAGO FERREIRA ARANHA              1715310047
+# VÍTOR SIMÕES AZEVEDO               1715310025
+# VICTOR HUGO DE OLIVEIRA CARREIRA   1715310063
+# REINALDO VARGAS                    1715310054
+#
 #7)Escreva um algoritmo em PORTUGOL que imprima todos os números pares do
 #intervalo fechado de 1 a 100.
+#
 
 c = 2
 a = 102
