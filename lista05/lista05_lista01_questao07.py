384a00a984c0e21ad2b03250b1301b502f818436
VictorHuguw <kibedasarabia@gmail.com>
2017-04-06 21:29:56 -0400

    Create lista05_lista01_questao07.py

---
+++ lista05/lista05_lista01_questao07.py
@@ -0,0 +1,5 @@
+numb1 = int(input("digite um numero"))
+numb2 = int(input("digite mais um numero"))
+numb3 = int(input("digite mais um numero"))
+numb4 = int(input("digite mais um numero"))
+numb5 = int(input("digite mais um numero"))
