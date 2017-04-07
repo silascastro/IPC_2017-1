50fe4b65552e5c90d33c57c7b3005dbd9706a104
VictorHuguw <kibedasarabia@gmail.com>
2017-04-06 21:41:55 -0400

    Update lista05_lista01_questao07.py

--- lista05/lista05_lista01_questao07.py
+++ lista05/lista05_lista01_questao07.py
@@ -14,7 +14,7 @@ while numb3 > numb1 and numb3 > numb2 and numb3 > numb4 and numb3 > numb5:
     print("o maior número é o: ", numb3)
     break
 while numb4 > numb1 and numb4 > numb2 and numb4 > numb3 and numb4 > numb5:
-    print("o maior número é o", numb4)
+    print("o maior número é o: ", numb4)
     break
 while numb5 > numb1 and numb5 > numb2 and numb5 > numb3 and numb5 > numb4:
     print("o maior número é o", numb5)
