ca50d749f10b0cbf46429ef048e3917c8d0c6eee
VictorHuguw <kibedasarabia@gmail.com>
2017-04-07 00:33:52 -0400

    Update lista05_lista01_questao07.py

--- lista05/lista05_lista01_questao07.py
+++ lista05/lista05_lista01_questao07.py
@@ -1,5 +1,6 @@
 # --------------------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
 number1 = int(input("digite um número: "))
 number2 = int(input("digite mais um número: "))
 number3 = int(input("digite mais um número: "))
