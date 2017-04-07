d8e81f16ffb2e8a7fc9e1d1d080c464f61f4fc2c
Iury S.C <coelhosc13@gmail.com>
2017-04-07 19:56:30 -0400

    Update lista05_lista01_questao25.py

--- lista05/lista05_lista01_questao25.py
+++ lista05/lista05_lista01_questao25.py
@@ -5,9 +5,10 @@
 # Adham Lucas da Silva Oliveira           1715310059
 # Alexandre Marques Uchôa                 1715310028
 # André Luís Laborda Neves                1515070006
-# Carlos Eduardo Tapudima de Oliveira	    1715310030
+# Carlos Eduardo Tapudima de Oliveira	  1715310030
 # Aracille de Souza Barbosa               1315120206
 # Dayana Picanço Marquez                  1715310058
+#
 #Faça um programa que peça para n pessoas a sua idade, ao final o programa devera verificar se a média de idade da turma 
 #varia entre 0 e 25,26 e 60 e maior que 60; e então, dizer se a turma é jovem, adulta ou idosa, conforme a média calculada. 
 #----------------------------------------------------------------------------------------------------------------------------------
