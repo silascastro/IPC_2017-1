1238810d87f845de1218ddaedca7046f4ec61297
reinaldo22 <vargas.reynaldo22@gmail.com>
2017-04-06 23:58:13 -0400

    Add files via upload
    exercicio15

---
+++ lista05/lista05_lista01_questao15.py
@@ -0,0 +1,26 @@
+----------------------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+#
+#Ulisses Antonio Da Costa                1515090555
+#Thiago ferreira Aranha 		 1715310047 
+#vitor simões Azevedo			 1715310025	 			  
+#victor hugo de oliveira carreira	 1715310063
+#Reinaldo vargas 			 1715310054
+
+#---------------------------------------------------------------------------------------------------
+
+#A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,...
+#Faça um programa capaz de gerar a série até o n−ésimo termo
+
+previous = 1
+next = 1
+fibonacci = 0
+
+while fibonacci < 55:
+
+    fibonacci = previous
+    previous = next
+    next = fibonacci + previous
+
+    print(fibonacci, end="...")
