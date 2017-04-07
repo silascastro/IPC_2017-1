4e103170642ec987fd382624b791de19f2e4d32f
Evandro Padilha Barroso FIlho <evandro7047301@gmail.com>
2017-04-06 21:33:07 -0400

    Update lista05_lista01_questao19.py

--- lista05/lista05_lista01_questao19.py
+++ lista05/lista05_lista01_questao19.py
@@ -1,3 +1,17 @@
+#----------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Evandro Padilha Barroso Filho     1715310009 
+# Gabriel Barroso da Silva Lima     1715310011
+# Frederico Victor Alfaia Rodrigues 1515200030 
+# Felipe Eduardo Silva de Almeida   1715310031 
+# Felipe Guerreiro De Mello         1315120052 
+# Silas castro de Mendonça          1715310066
+#
+#19.Altere o programa anterior para que ele aceite apenas números entre 0 e 1000.
+#
+#----------------------------------------------------------------
 #Altere o programa anterior para que ele aceite apenas números entre 0 e 1000.
 n_numbers = float(input("Digite o tamanho de um conjunto (números de 0-10000): "))
 
