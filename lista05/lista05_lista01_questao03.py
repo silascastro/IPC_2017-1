6a185a1542c82a57fc635454ed977abb8d868835
Iury S.C <coelhosc13@gmail.com>
2017-04-07 18:52:08 -0400

    Update lista05_lista01_questao03.py

--- lista05/lista05_lista01_questao03.py
+++ lista05/lista05_lista01_questao03.py
@@ -8,6 +8,7 @@
 # Felipe Eduardo Silva de Almeida   1715310031 
 # Felipe Guerreiro De Mello         1315120052 
 # Silas castro de Mendonça          1715310066
+#
 #03.Faça um programa que leia e valide as seguintes informações:
 #Nome: maior que 3 caracteres;
 #Idade: entre 0 e 150;
@@ -16,27 +17,27 @@
 #Estado Civil: 's', 'c', 'v', 'd';
 #----------------------------------------------------------------
 
-name = input("Digite seu nome (min: 3 caracteres): ")
+name = input("Digite seu nome (mín.: 3 caracteres): ")
 while len(name) < 3:
-    print("Campo invalido :")
-    name = input("Digite seu nome (min: 3 caracteres): ")
+    print("Campo inválido: ")
+    name = input("Digite seu nome (mín.: 3 caracteres): ")
 
 age = int(input("Digite sua idade: "))
 while age < 0 or age > 150:
-    print("Campo invalido :")
+    print("Campo inválido: ")
     age = int(input("Digite sua idade: "))
 
 sallary = float(input("Digite seu salário: "))
 while sallary <= 0:
-    print("Campo invalido :")
+    print("Campo inválido :")
     sallary = float(input("Digite seu salário: "))
 
 sex = input("Digite seu sexo usando f para feminino e m para masculino: ")
 while sex != 'f' and sex != 'm':
-    print("Campo invalido :")
+    print("Campo inválido: ")
     sex = input("Digite seu sexo usando f para feminino e m para masculino: ")
 
-civil_state = input("Digite seu estado civil(s, c, v, d): ")
+civil_state = input("Digite seu estado civil (s, c, v, d): ")
 while civil_state != 's' and civil_state != 'c' and civil_state != 'v' and civil_state != 'd':
     civil_state = input("Digite seu estado civil(s, c, v, d): ")
 
