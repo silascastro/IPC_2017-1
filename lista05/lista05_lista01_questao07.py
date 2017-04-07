a4fdb635111b36ed91522a08bb885e8641c102d9
erikatilio <erikatilio@users.noreply.github.com>
2017-04-07 10:22:14 -0400

    Ajuste da questão

--- lista05/lista05_lista01_questao07.py
+++ lista05/lista05_lista01_questao07.py
@@ -9,32 +9,41 @@
 # REINALDO VARGAS                    1715310054
 #
 #7)Faça um programa que leia 5 números e informe o maior número.
-#
-number1 = int(input("digite um número: "))
-number2 = int(input("digite mais um número: "))
-number3 = int(input("digite mais um número: "))
-number4 = int(input("digite mais um número: "))
-number5 = int(input("digite mais um número: "))
+#---------------------------------------------------------------------------
 
-while number1 > number2 and number1 > number3 and number1 > number4 and number1 > number5:
-    print("o maior número é o: ", number1)
-    break
-    
-while number2 > number1 and number2 > number3 and number2 > number4 and number2 > number5:
-    print("o maior número é o: ", number2)
-    break
-    
-while number3 > number1 and number3 > number2 and number3 > number4 and number3 > number5:
-    print("o maior número é o: ", number3)
-    break
-    
-while number4 > number1 and number4 > number2 and number4 > number3 and number4 > number5:
-    print("o maior número é o: ", number4)
-    break
-    
-while number5 > number1 and number5 > number2 and number5 > number3 and number5 > number4:
-    print("o maior número é o: ", number5)
-    break
-    
+print("Informe 5 números para imprimir o maior deles")
+
+number1 = float(input("Digite um número: "))
+number2 = float(input("Digite um número: "))
+number3 = float(input("Digite um número: "))
+number4 = float(input("Digite um número: "))
+number5 = float(input("Digite um número: "))
+
+if number1 > number2 and number1 > number3 and number1 > number4 and number1 > number5:
+    while number1 > number2 and number1 > number3 and number1 > number4 and number1 > number5:
+        print("o maior número é o: ", number1)
+        break
+
+if number2 > number1 and number2 > number3 and number2 > number4 and number2 > number5:
+    while number2 > number1 and number2 > number3 and number2 > number4 and number2 > number5:
+        print("o maior número é o: ", number2)
+        break
+
+if number3 > number1 and number3 > number2 and number3 > number4 and number3 > number5:
+    while number3 > number1 and number3 > number2 and number3 > number4 and number3 > number5:
+        print("o maior número é o: ", number3)
+        break
+
+if number4 > number1 and number4 > number2 and number4 > number3 and number4 > number5:
+    while number4 > number1 and number4 > number2 and number4 > number3 and number4 > number5:
+        print("o maior número é o: ", number4)
+        break
+
+if number5 > number1 and number5 > number2 and number5 > number3 and number5 > number4:
+    while number5 > number1 and number5 > number2 and number5 > number3 and number5 > number4:
+        print("o maior número é o: ", number5)
+        break
+
 else:
-    print("operação invalida! números iguais")
+    if number1 == number2 == number3 == number4 == number5:
+        print("operação invalida! números iguais")
