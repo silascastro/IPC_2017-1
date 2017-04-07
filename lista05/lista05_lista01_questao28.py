de221ca6ae2104bcb8d6e70de648a90713fb2594
Joelson Lima <joelson174@users.noreply.github.com>
2017-04-07 12:26:28 -0400

    Update lista05_lista01_questao28.py

--- lista05/lista05_lista01_questao28.py
+++ lista05/lista05_lista01_questao28.py
@@ -1,3 +1,17 @@
+#---------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+#
+# Joelson Pereira Lima 			    1715310060
+# Gabriel Nascimento de Oliveira 	1715310052
+# Hugo Thadeu Silva Cardoso         1715310013
+# Jandinne Duarte de Oliveira 		1015070265
+# Gabriel de Queiroz Souza		    1715310044
+# Guilherme Silva de Oliveira		1715310034
+#
+# Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada
+#um deles. O usuário deverá informar a quantidade de CDs e o valor para em cada um. 
+
 quantidade = 0
 
 while quantidade <= 0:
