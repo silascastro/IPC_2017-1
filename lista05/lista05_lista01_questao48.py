a937b10b8891a27c165a02dfe1164ed5fa273e7c
Walter Nobre <walternobre15@gmail.com>
2017-04-08 13:18:40 -0400

    G8

---
+++ lista05/lista05_lista01_questao48.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Grupo 08
+# Victor Summer Oliveira Pantaleao 1715310042
+# Walter Nobre Da Silva Conceição  1715310057
+# Wesley da Silva Rocha            1715310026
+# Wilbert Luís Evangelista Martins 1715310055
+# Yuri Leandro de Aquino Silva     1615100462
+#
+# Faça um programa que peça um numero inteiro positivo e em seguida mostre este numero invertido.
+# ----------------------------------------------------------
+
+print(str(input('write a number with more than one number: '))[::-1])
