c5855f1ca2e133c57dbe5e312e1a9607c466b0d0
alexandreuch <alexandreuch@gmail.com>
2017-04-08 16:16:56 -0400

    Update lista05_lista01_questao33.py

--- lista05/lista05_lista01_questao33.py
+++ lista05/lista05_lista01_questao33.py
@@ -1,3 +1,14 @@
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+#
+# Adham Lucas da Silva Oliveira           1715310059
+# Alexandre Marques Uchôa                 1715310028
+# André Luís Laborda Neves                1515070006
+# Carlos Eduardo Tapudima de Oliveira	  1715310030
+# Aracille de Souza Barbosa               1315120206
+# Dayana Picanço Marquez                  1715310058
+#
 #O Departamento Estadual de Meteorologia lhe contratou para desenvolver um programa que leia um conjunto indeterminado
 #de temperaturas, e informe ao final a menor e a maior temperaturas informadas, bem como a média das temperaturas.
 #-----------------------------------------------------------------------------------------------------------------------
