540effcf512e8bc41bd0edb16c3113aafec5a4a6
Gabriel Barroso <gabriel.barroso.am@gmail.com>
2017-04-08 21:32:41 -0400

    Comitando questão do grupo 3

---
+++ lista05/lista05_lista01_questao51.py
@@ -0,0 +1,25 @@
+#----------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Evandro Padilha Barroso Filho     1715310009
+# Gabriel Barroso da Silva Lima     1715310011
+# Frederico Victor Alfaia Rodrigues 1515200030
+# Felipe Eduardo Silva de Almeida   1715310031
+# Felipe Guerreiro De Mello         1315120052
+# Silas castro de Mendonça          1715310066
+# Faça um programa que mostre os n termos da Série a seguir:
+# S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m.
+# Imprima no final a soma da série.
+
+lim = int(input("quantidade de termos: "))
+S = 0
+m = 1
+n = 1
+
+while m <= lim:
+    S += m / n
+    m += 1
+    n += 2
+
+print("Soma:", S)
