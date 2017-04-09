7e7040073bde78ed0efaca858c2c78d2ca27cfa6
Joelson Lima <joelson174@users.noreply.github.com>
2017-04-09 01:16:01 -0400

    Update lista05_lista01_questao28.py

--- lista05/lista05_lista01_questao28.py
+++ lista05/lista05_lista01_questao28.py
@@ -12,19 +12,19 @@
 # Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada
 #um deles. O usuário deverá informar a quantidade de CDs e o valor para em cada um. 
 
-quantidade = 0
+quantity = 0
 
-while quantidade <= 0:
-    quantidade = int(input("Digite a quantidade de CDs: "))
-    if quantidade <= 0:
+while quantity <= 0:
+    quantity = int(input("Digite a quantidade de CDs: "))
+    if quantity <= 0:
         print("Quantidade deve ser positiva e diferente de 0")
 
-valor = 0
-while valor <=0:
-    valor = float(input("Digite o preço dos CDs: "))
-    if valor <= 0:
+value = 0
+while value <=0:
+    value = float(input("Digite o preço dos CDs: "))
+    if value <= 0:
         print("Valor deve ser positivo e diferente de 0")
-average = valor/quantidade
+average = value/quantity
 
-print("Quantidade de CDs é ", quantidade)
+print("Quantidade de CDs é ", quantity)
 print("Preço médio dos CDs é de R$" '%0.2f' %average)
