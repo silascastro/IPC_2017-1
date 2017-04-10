66a6f360e1321979b3aade37a84626b4f8f66cd4
KethelenTamara <kettamara@gmail.com>
2017-04-10 00:21:26 -0400

    Update lista05_lista01_questao29.py

--- lista05/lista05_lista01_questao29.py
+++ lista05/lista05_lista01_questao29.py
@@ -1,3 +1,11 @@
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------
 #O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca de 10 caixas. Para agilizar o cálculo de quanto cada cliente
 #deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente comprou e ao lado o valor da conta. Desta forma a atendente
 #o caixa precisa apenas contar quantos itens o cliente está levando e olhar na tabela de preços. Você foi contratado para desenvolver
