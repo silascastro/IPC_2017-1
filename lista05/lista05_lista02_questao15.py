28da920f40b05346d78e1396825c0640a2887250
Tiago Aranha <tiagoweb@gmail.com>
2017-04-10 22:00:38 -0400

    Update lista05_lista02_questao15.py

--- lista05/lista05_lista02_questao15.py
+++ lista05/lista05_lista02_questao15.py
@@ -1,98 +1,22 @@
-
-#Introdução a Programação de Computadores - IPC
-#Universidade do Estado do Amazonas - UEA
-#
-#Ulisses Antonio Da Costa             1515090555
-#Thiago ferreira Aranha 		  1715310047
-#vitor simões Azevedo			  1715310025
-#victor hugo de oliveira carreira	  1715310063
-#Reinaldo vargas 			      1715310054
-#---------------------------------------------------------------------------------------------------
-#Escreva um algoritmo em PORTUGOL que receba oito números do usuário e imprima
-#o logaritmo de cada um deles na base 10.
-
+# --------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# ULISSES ANTONIO ANTONINO DA COSTA  1515090555
+# TIAGO FERREIRA ARANHA              1715310047
+# VÍTOR SIMÕES AZEVEDO               1715310025
+# VICTOR HUGO DE OLIVEIRA CARREIRA   1715310063
+# REINALDO VARGAS                    1715310054
+# 15. Escreva um algoritmo em PORTUGOL que receba oito números do usuário e imprima
+# o logaritmo de cada um deles na base 10.
+# --------------------------------------------------------------------------
 
 import math
 
-print("=1================")
+c = 1
 
-      input_log1 = int(input("dgite seu primeiro numero:"))
-while input_log1 > 0 :
-      input_log1 = math.log10(input_log1)
-      input_log1 = int(input("dgite seu primeiro numero:"))
-      print(input_log1)
-if input_log1 == 0:
-      input_log1 = int(input("dgite seu primeiro numero:"))
-      input_log1 = math.log10(input_log1)
-      print(input_log1)
-
-print("=2================")
-      input_log2 = int(input("dgite seu segundo numero:"))
-while input_log2 > 0 :
-      input_log2 = math.log10(input_log2)
-      print(entrada_log2)
-if input_log2 == 0:
-      input_log2 = int(input("dgite seu segundo numero:"))
-      input_log2 = math.log10(input_log2)
-      print(input_log2)
-
-print("==3===============")
-      input_log3 = int(input("dgite seu terceiro numero:"))
-while input_log3 > 0:
-      input_log3 = math.log10(input_log3)
-      print(input_log3)
-if input_log3 ==0:
-      input_log3 = int(input("dgite seu terceiro numero:"))
-      input_log3 = math.log10(input_log3)
-      print(input_log3)
-
-print("===4==============")
-      input_log4 = int(input("dgite seu quarto numero:"))
-while input_log4 > 0 :
-      input_log4 = math.log10(input_log4)
-      print(input_log4)
-if input_log4 == 0:
-      input_log4 = int(input("dgite seu quarto numero:"))
-      input_log4 = math.log10(input_log4)
-      print(input_log4)
-
-print("====5=============")
-      input_log5 = int(input("dgite seu quinto numero:"))
-while input_log5 > 0 :
-      input_log5 = math.log10(input_log5)
-      print(input_log5)
-if input_log5 ==0:
-      input_log5 = int(input("dgite seu quinto numero:"))
-      input_log5 = math.log10(input_log5)
-      print(input_log5)
-
-print("=====6============")
-      input_log6 = int(input("dgite seu sexto numero:"))
-while input_log6 > 0:
-      input_log6 = math.log10(input_log6)
-      print(entrada_log6)
-if input_log6 == 0:
-      input_log6 = int(input("dgite seu sexto numero:"))
-      input_log6 = math.log10(input_log6)
-      print(input_log6)
-
-print("======7===========")
-      input_log7 = int(input("dgite seu sétimo numero:"))
-while input_log7 > 0 :
-      input_log7 = math.log10(input_log7)
-      print(input_log7)
-if input_log7 == 0:
-      input_log7 = int(input("dgite seu sétimo numero:"))
-      input_log7 = math.log10(input_log7)
-      print(input_log7)
-
-print("=======8==========")
-      input_log8 = int(input("dgite seu oitavo numero:"))
-while input_log8 > 0:
-      input_log8 = math.log10(input_log8)
-      print(input_log8)
-if input_log8 == 0:
-      input_log8 = int(input("dgite seu oitavo numero:"))
-      input_log8 = math.log10(input_log8)
-      print(input_log8)
-print("CALCULO DE LOGARÍTMOS BASE 10 FINALIZADO")
+while c <= 8:
+    numero = float(input("Número {}: ".format(c)))
+    log = math.log10(numero)
+    print("Logaritmo de %5.2f na base 10 é %5.2f" % (numero, log))
+    c += 1
