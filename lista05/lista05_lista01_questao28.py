371e57e473ea14e392683bd1d8f054767f46309a
Tiago Aranha <tiagoweb@gmail.com>
2017-04-11 07:36:17 -0400

    Update lista05_lista01_questao28.py

--- lista05/lista05_lista01_questao28.py
+++ lista05/lista05_lista01_questao28.py
@@ -1,7 +1,7 @@
 #---------------------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
-#
+# Prof. Jucimar Jr
 # Joelson Pereira Lima 			    1715310060
 # Gabriel Nascimento de Oliveira 	1715310052
 # Hugo Thadeu Silva Cardoso         1715310013
@@ -9,22 +9,23 @@
 # Gabriel de Queiroz Souza		    1715310044
 # Guilherme Silva de Oliveira		1715310034
 #
-# Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada
-#um deles. O usuário deverá informar a quantidade de CDs e o valor para em cada um. 
+# 28. Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada
+# um deles. O usuário deverá informar a quantidade de CDs e o valor para em cada um. 
+#---------------------------------------------------------------------------
 
-quantity = 0
+quantidade_de_cds = int(input("Informe a quantidade de CDs: "))
 
-while quantity <= 0:
-    quantity = int(input("Digite a quantidade de CDs: "))
-    if quantity <= 0:
-        print("Quantidade deve ser positiva e diferente de 0")
+c = 1
+valor_total_gasto = 0
 
-value = 0
-while value <=0:
-    value = float(input("Digite o preço dos CDs: "))
-    if value <= 0:
-        print("Valor deve ser positivo e diferente de 0")
-average = value/quantity
+while c <= quantidade_de_cds:
 
-print("Quantidade de CDs é ", quantity)
-print("Preço médio dos CDs é de R$" '%0.2f' %average)
+    valor_cd = float(input("Valor do {}º CD: ".format(c)))
+    valor_total_gasto = valor_total_gasto + valor_cd
+
+    c += 1
+
+valor_medio_gasto = valor_total_gasto / quantidade_de_cds
+
+print("Valor total investido: %5.2f" % valor_total_gasto)
+print("Valor médio gasto em cada CD: %5.2f" % valor_medio_gasto)
