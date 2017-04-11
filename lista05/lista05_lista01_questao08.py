d090277d9d6fa3a8e2a46836ee7831fede1a9286
Wilbert Marins <wilbertluis2014@gmail.com>
2017-04-11 01:55:44 -0400

    correção de nome

--- lista05/lista05_lista01_questao08.py
+++ lista05/lista05_lista01_questao08.py
@@ -6,7 +6,7 @@
 # Victor Summer Oliveira Pantaleao 1715310042
 # Walter Nobre Da Silva Conceição  1715310057
 # Wesley da Silva Rocha            1715310026
-# Wilbert Luís Evangelista Martins 1715310055
+# Wilbert Luís Evangelista Marins  1715310055
 # Yuri Leandro de Aquino Silva     1615100462
 #
 #Faça um programa que leia 5 números e informe a soma e a mé-
