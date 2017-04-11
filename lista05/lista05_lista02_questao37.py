0ae4efa75d7dcf8b646a929a8f4ed3223ac4cd41
uaadc <llulissesll@gmail.com>
2017-04-11 00:11:07 -0400

    lista05_lista02_questao37.py
    created lista05_lista02_questao37.py

---
+++ lista05/lista05_lista02_questao37.py
@@ -0,0 +1,25 @@
+# --------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# ULISSES ANTONIO ANTONINO DA COSTA  1515090555
+# TIAGO FERREIRA ARANHA              1715310047
+# VÍTOR SIMÕES AZEVEDO               1715310025
+# VICTOR HUGO DE OLIVEIRA CARREIRA   1715310063
+# REINALDO VARGAS                    1715310054
+#
+# 37) Escreva um algoritmo em PORTUGOL que determine todos os divisores de
+# um dado número N.
+#---------------------------------------------------------------------------
+
+n=int(input("number: "))
+c=1   #aux
+
+print("Dividers: ")
+
+while c<=n:
+    
+    if n%c==0:
+        print(c)
+
+    c=c+1
