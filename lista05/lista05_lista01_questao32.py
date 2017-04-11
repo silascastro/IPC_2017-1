c69fcc5eb7ed37bed0b6c8b432921e978aaab6d3
Wesley Rocha <wsr.snf17@uea.edu.br>
2017-04-11 02:50:08 -0400

    Update lista05_lista01_questao32.py

--- lista05/lista05_lista01_questao32.py
+++ lista05/lista05_lista01_questao32.py
@@ -10,16 +10,18 @@
 # Yuri Leandro de Aquino Silva     1615100462
 #
 # Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
-# Ex.: 5!=5.4.3.2.1=120. A saída deve ser conforme o exemplo abaixo:
+# A saída deve ser conforme o exemplo abaixo:
+# 5! = 5 . 4 . 3 . 2 . 1 = 120
 # ----------------------------------------------------------
+n = int(input())
 
-fat = 1
-num = int(input('Write a number: '))
-while num > 0:
-    if num <= 0:
-        print(num)
-    else:
-        print(num)
-    fat = num * fat
-    num = num - 1
-print(fat)
+print('Fatorial de:', n)
+print('{}! = {}'.format(n, n), end = ' ')
+
+factorial = n
+n -= 1
+while n > 0:
+    factorial *= n
+    print('.', n, end = ' ')
+    n -= 1
+print('=', factorial)
