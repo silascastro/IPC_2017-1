6c460aa0cdd82ae097f56dc827a24b122da3e779
Felipe Almeida <edu468felipe@gmail.com>
2017-04-12 15:08:33 -0400

    Update lista05_lista02_questao35.py

--- lista05/lista05_lista02_questao35.py
+++ lista05/lista05_lista02_questao35.py
@@ -1,3 +1,20 @@
+#----------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Evandro Padilha Barroso Filho     1715310009
+# Gabriel Barroso da Silva Lima     1715310011
+# Frederico Victor Alfaia Rodrigues 1515200030
+# Felipe Eduardo Silva de Almeida   1715310031
+# Felipe Guerreiro De Mello         1315120052
+# Silas castro de Mendonça          1715310066
+#
+# Escreva um algoritmo em PORTUGOL que determine se dois valores inteiros e
+# positivos A e B são primos entre si. (dois números inteiros são ditos primos entre si,
+# caso não exista divisor comum aos dois números). 
+#----------------------------------------------------------------
+
 number_a = float(input("Digite um numero: "))
 number_b = float(input("Digite outro numero: "))
 c = 1
