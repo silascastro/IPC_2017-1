8860bd7189467f6e22b8ddf70e32278c417c55ee
Felipe Almeida <edu468felipe@gmail.com>
2017-04-12 11:10:54 -0400

    Update lista05_lista01_questao12.py

--- lista05/lista05_lista01_questao12.py
+++ lista05/lista05_lista01_questao12.py
@@ -8,13 +8,13 @@
 # Diego Reis figueira           1515070169
 # Iury da Silva Coelho          1715310069
 #
-#12.Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
-#O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
-#Tabuada de 5:
-#5 X 1 = 5
-#5 X 2 = 10
-#...
-#5 X 10 = 50
+# 12.Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+# O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
+# Tabuada de 5:
+# 5 X 1 = 5
+# 5 X 2 = 10
+# ...
+# 5 X 10 = 50
 #-----------------------------------------------------------------------------------------------------------------------
 
 number = int(input("Qual número você deseja ver a tabuada?: "))
