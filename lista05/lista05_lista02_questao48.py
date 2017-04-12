ddcdd1b7d91095d6059c2e7da30ed51b7142915f
Felipe Almeida <edu468felipe@gmail.com>
2017-04-12 17:01:38 -0400

    Update lista05_lista02_questao48.py

--- lista05/lista05_lista02_questao48.py
+++ lista05/lista05_lista02_questao48.py
@@ -1,9 +1,26 @@
-a = int(input())
+#----------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Evandro Padilha Barroso Filho     1715310009
+# Gabriel Barroso da Silva Lima     1715310011
+# Frederico Victor Alfaia Rodrigues 1515200030
+# Felipe Eduardo Silva de Almeida   1715310031
+# Felipe Guerreiro De Mello         1315120052
+# Silas castro de Mendonça          1715310066
+#
+#48) Faça um algoritmo em PORTUGOL para calcular o valor de S, dado por:
+# s = (1/n)+(2/(n-1)...(n/1)
+# sendo N fornecido pelo teclado. 
+#
+#----------------------------------------------------------------
+number_n = int(input())
 count = 1
 s = 0
-final = a
+final = number_n
 while count <= final:
-    s = s + (count/a)
+    s = s + (count/number_n)
     count += 1
-    a -= 1
+    number_n -= 1
 print (s)
