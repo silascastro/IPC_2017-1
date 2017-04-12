1a55ca9cec3d9bae2eb2d1fb1a6d2f990137b606
Gabriel Queiroz <gabriel.dequeirozsousa@gmail.com>
2017-04-12 17:40:45 -0400

    Update lista05_lista02_questao73.py

--- lista05/lista05_lista02_questao73.py
+++ lista05/lista05_lista02_questao73.py
@@ -1,9 +1,21 @@
+# -----------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Gabriel Nascimento de Oliveira    1715310052
+# Gabriel de Queiroz Souza          1715310044
+# Guilherme Silva de Oliveira       1715310034
+# Hugo Thadeu Silva Cardoso         1715310013
+# Jandinne Duarte de Oliveira       1015070265
+# Joelson Pereira Lima              1715310060
+#
 # Na usina de Angra dos Reis, os técnicos analisam a perda de massa de um material
 # radioativo. Sabendo-se que este perde 25% de sua massa a cada 30 segundos. Criar um
 # algoritmo em PORTUGOL que calcule iterativamente e imprima o tempo necessário
 # para que a massa deste material se torne menor que 0,10 grama. O algoritmo pode
 # calcular o tempo para várias massas.
-
+# -----------------------------------------------------------------------------------------
 while 1:
     massa = float(input("Informe massa do material em gramas: "))
     tempo = 0
@@ -22,4 +34,5 @@ while 1:
         elif op == 'n' or op == 'N':
             exit(0)
         else:
-            print("Opção invalida. Tente novamente!!")
+            print("Opção invalida. Tente novamente!!\n")
+
