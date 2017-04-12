7057f96be3d023cb64d27cb8f2ed6345e596def7
Felipe Almeida <edu468felipe@gmail.com>
2017-04-12 13:29:37 -0400

    Update lista05_lista01_questao04.py

--- lista05/lista05_lista01_questao04.py
+++ lista05/lista05_lista01_questao04.py
@@ -16,13 +16,13 @@
 # ou iguale a população do país B, mantidas as taxas de crescimento.
 #----------------------------------------------------------------
 
-pop_a = 80000
+population_a = 80000
 grow_a = 0.03
-pop_b = 200000
+population_b = 200000
 grow_b = 0.015
 c = 0
-while pop_a <= pop_b:
-    pop_a = pop_a + (pop_a * grow_a)
-    pop_b = pop_b + (pop_b * grow_b)
+while population_a <= population_b:
+    population_a = population_a + (population_a * grow_a)
+    population_b = population_b + (population_b * grow_b)
     c = c + 1
 print("Será preciso", c ,"anos para o país A ser mais populoso que o país B")
