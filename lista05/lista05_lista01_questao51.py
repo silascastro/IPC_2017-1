56692ad7391065b15d245637fb643b4d0fe165e5
Evandro Padilha Barroso Filho <evandro7047301@gmail.com>
2017-04-11 21:43:55 -0400

    Update lista05_lista01_questao51.py

--- lista05/lista05_lista01_questao51.py
+++ lista05/lista05_lista01_questao51.py
@@ -8,7 +8,7 @@
 # Felipe Eduardo Silva de Almeida   1715310031
 # Felipe Guerreiro De Mello         1315120052
 # Silas castro de Mendonça          1715310066
-# Faça um programa que mostre os n termos da Série a seguir:
+# 51.Faça um programa que mostre os n termos da Série a seguir:
 # S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m.
 # Imprima no final a soma da série.
 
