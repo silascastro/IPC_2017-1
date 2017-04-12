62111cab9887d9384c8ff7d6b3c643a89185e39c
Tiago Aranha <tiagoweb@gmail.com>
2017-04-11 21:20:25 -0400

    Update lista05_lista01_questao39.py

--- lista05/lista05_lista01_questao39.py
+++ lista05/lista05_lista01_questao39.py
@@ -19,29 +19,29 @@
 
 c = 1
 
-higher_student = ""
-higher_student_height = 0
+aluno_mais_alto = ""
+aluno_mais_alto_altura = 0
 
-lower_student = ""
-lower_student_height = 0
+aluno_mais_baixo = ""
+aluno_mais_baixo_altura = 0
 
-while c <= 2:
-    student_name = input("Informe o nome do aluno: ")
-    student_height = int(input("Informe a altura do aluno: "))
+while c <= 10:
+    aluno_nome = input("Informe o nome do aluno: ")
+    aluno_altura = int(input("Informe a altura do aluno: "))
 
     if c == 1:
-        higher_student_height = student_height
-        lower_student_height = student_height
-        higher_student = student_name
-        lower_student = student_name
+        aluno_mais_alto_altura = aluno_altura
+        aluno_mais_baixo_altura = aluno_altura
+        aluno_mais_alto = aluno_nome
+        aluno_mais_baixo = aluno_nome
     else:
-        if student_height > higher_student_height:
-            higher_student_height = student_height
-            higher_student = student_name
-        elif student_height < lower_student_height:
-            lower_student_height = student_height
-            lower_student = student_name
+        if aluno_altura > aluno_mais_alto_altura:
+            aluno_mais_alto_altura = aluno_altura
+            aluno_mais_alto = aluno_nome
+        elif aluno_altura < aluno_mais_baixo_altura:
+            aluno_mais_baixo_altura = aluno_altura
+            aluno_mais_baixo = aluno_nome
     c = c + 1
 
-print("O aluno mais alto é %s e mede %5.2f" % (higher_student, higher_student_height))
-print("O aluno mais baixo é %s e mede %5.2f" % (lower_student, lower_student_height))
+print("O aluno mais alto é %s e mede %5.2f" % (aluno_mais_alto, aluno_mais_alto_altura))
+print("O aluno mais baixo é %s e mede %5.2f" % (aluno_mais_baixo, aluno_mais_baixo_altura))
