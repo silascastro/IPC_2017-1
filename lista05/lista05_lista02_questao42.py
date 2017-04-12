3618031cc0cda5c1b871abe180c3044c6d39b7af
Felipe Almeida <edu468felipe@gmail.com>
2017-04-12 15:19:16 -0400

    Update lista05_lista02_questao42.py

--- lista05/lista05_lista02_questao42.py
+++ lista05/lista05_lista02_questao42.py
@@ -1,3 +1,23 @@
+#----------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Evandro Padilha Barroso Filho     1715310009
+# Gabriel Barroso da Silva Lima     1715310011
+# Frederico Victor Alfaia Rodrigues 1515200030
+# Felipe Eduardo Silva de Almeida   1715310031
+# Felipe Guerreiro De Mello         1315120052
+# Silas castro de Mendonça          1715310066
+#
+# 42) A série de RICCI difere da série de FIBONACCI porque os dois primeiros termos são
+# fornecidos pelo usuário. Os demais termos são gerados da mesma forma que a série de
+# FIBONACCI. Criar um algoritmo em PORTUGOL que imprima os N primeiros
+# termos da série de RICCI e a soma dos termos impressos, sabendo-se que para existir
+#esta série serão necessários pelo menos três termos. 
+#
+#----------------------------------------------------------------
+
 prev_number = int(input("Digite um numero: "))
 next_number = int(input("Digite outro numero: "))
 fibonacci = 0
