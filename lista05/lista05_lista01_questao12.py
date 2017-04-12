609d3baaa3995bd37b270f8b0736e42b4213fe6e
Erik Atilio Silva Rey <erikatilio@users.noreply.github.com>
2017-04-12 08:51:16 -0400

    Grupo 02

---
+++ lista05/lista05_lista01_questao12.py
@@ -0,0 +1,27 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Erik Atilio Silva Rey         1715310059
+# Edson de Lima Barros          1715310043
+# Enrique Leão Barbosa Izel     1715310048
+# Diego Reis figueira           1515070169
+# Iury da Silva Coelho          1715310069
+#
+#12.Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+#O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
+#Tabuada de 5:
+#5 X 1 = 5
+#5 X 2 = 10
+#...
+#5 X 10 = 50
+#-----------------------------------------------------------------------------------------------------------------------
+
+number = int(input("Qual número você deseja ver a tabuada?: "))
+
+c = 1
+
+while c < 11:
+    result = number*c
+    print(number,"X",c,"=",result)
+    c = c + 1
