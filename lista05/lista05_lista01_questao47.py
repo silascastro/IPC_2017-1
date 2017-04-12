3cd02acfe7918ed8028436a7b9e3828d0aca35b3
Tiago Aranha <tiagoweb@gmail.com>
2017-04-11 21:21:47 -0400

    Update lista05_lista01_questao47.py

--- lista05/lista05_lista01_questao47.py
+++ lista05/lista05_lista01_questao47.py
@@ -33,41 +33,6 @@
 # Média: 9,04
 # -----------------------------------------------------------------------------
 
-# encoding: utf-8
-# --------------------------------------------------------------------------
-# Introdução a Programação de Computadores - IPC
-# Universidade do Estado do Amazonas - UEA
-# Prof. Jucimar Jr
-# ULISSES ANTONIO ANTONINO DA COSTA - 1515090555
-# TIAGO FERREIRA ARANHA - 1715310047
-# VÍTOR SIMÕES AZEVEDO - 1715310025
-# VICTOR HUGO DE OLIVEIRA CARREIRA - 1715310063
-# REINALDO VARGAS - 1715310054
-#
-# Em uma competição de ginástica, cada atleta recebe votos de sete jurados.
-# A melhor e a pior nota são eliminadas. A sua nota fica sendo a média dos votos restantes.
-# Você deve fazer um programa que receba o nome do ginasta e as notas dos sete jurados
-# alcançadas pelo atleta em sua apresentação e depois informe a sua média, conforme
-# a descrição acima informada (retirar o melhor e o pior salto e depois calcular
-# a média com as notas restantes). As notas não são informados ordenadas.
-# Um exemplo de saída do programa deve ser conforme o exemplo abaixo:
-#
-# Atleta: Aparecido Parente
-# Nota: 9.9
-# Nota: 7.5
-# Nota: 9.5
-# Nota: 8.5
-# Nota: 9.0
-# Nota: 8.5
-# Nota: 9.7
-#
-# Resultado final:
-# Atleta: Aparecido Parente
-# Melhor nota: 9.9
-# Pior nota: 7.5
-# Média: 9,04
-# -----------------------------------------------------------------------------
-
 nome_ginasta = input("Informe o nome do ginasta: ")
 
 c = 1
