e096a45742183fb756810b863fcfb3cac114c2fb
Guilherme Oliveira <guilhermesilva609@gmail.com>
2017-04-12 16:33:52 -0400

    fon

--- lista05/lista05_lista01_questao36.py
+++ lista05/lista05_lista01_questao36.py
@@ -1,3 +1,18 @@
+# -----------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Gabriel Nascimento de Oliveira    1715310052
+# Gabriel de Queiroz Souza          1715310044
+# Guilherme Silva de Oliveira       1715310034
+# Hugo Thadeu Silva Cardoso         1715310013
+# Jandinne Duarte de Oliveira       1015070265
+# Joelson Pereira Lima              1715310060
+#
+# Altere o programa de cálculo do fatorial, permitindo ao usuário calcular o 
+# fatorial várias vezes e limitando o fatorial números inteiros positivos e menores que 16.
+# -----------------------------------------------------------------------------------------
 a = int(input())
 b = int(input())
 c = int(input())
