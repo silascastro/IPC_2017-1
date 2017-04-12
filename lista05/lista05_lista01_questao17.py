363b47bbc841d06057d86d601482e4fed8e592cc
Felipe Almeida <edu468felipe@gmail.com>
2017-04-12 11:48:27 -0400

    Update lista05_lista01_questao17.py

--- lista05/lista05_lista01_questao17.py
+++ lista05/lista05_lista01_questao17.py
@@ -10,10 +10,8 @@
 # Aracille de Souza Barbosa               1315120206
 # Dayana Picanço Marquez                  1715310058
 #
-#Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
-#----------------------------------------------------------------------------
-#Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
-#  Ex.: 5!=5.4.3.2.1=120
+# Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+#   Ex.: 5!=5.4.3.2.1=120
 #----------------------------------------------------------------------------
 
 number = int(input("Informe um número: "))
