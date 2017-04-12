f81153e01d89ba0dc2b23ab505a32bca7fa4de2e
Felipe Almeida <edu468felipe@gmail.com>
2017-04-12 16:40:05 -0400

    Update lista05_lista01_questao36.py

--- lista05/lista05_lista01_questao36.py
+++ lista05/lista05_lista01_questao36.py
@@ -1,23 +1,35 @@
-# -----------------------------------------------------------------------------------------
+#----------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
 #
-# Gabriel Nascimento de Oliveira    1715310052
-# Gabriel de Queiroz Souza          1715310044
-# Guilherme Silva de Oliveira       1715310034
-# Hugo Thadeu Silva Cardoso         1715310013
-# Jandinne Duarte de Oliveira       1015070265
-# Joelson Pereira Lima              1715310060
+# Evandro Padilha Barroso Filho     1715310009
+# Gabriel Barroso da Silva Lima     1715310011
+# Frederico Victor Alfaia Rodrigues 1515200030
+# Felipe Eduardo Silva de Almeida   1715310031
+# Felipe Guerreiro De Mello         1315120052
+# Silas castro de Mendonça          1715310066
 #
-# Altere o programa de cálculo do fatorial, permitindo ao usuário calcular o 
-# fatorial várias vezes e limitando o fatorial números inteiros positivos e menores que 16.
-# -----------------------------------------------------------------------------------------
-a = int(input())
-b = int(input())
-c = int(input())
-count = b
+# Desenvolva um programa que faça a tabuada de um número qualquer 
+# inteiro que será digitado pelo usuário, mas a tabuada não deve 
+# necessariamente iniciar em 1 e terminar em 10, o valor inicial e 
+# final devem ser informados também pelo usuário, conforme exemplo abaixo:
+#   Montar a tabuada de: 5
+#   Começar por: 4
+#   Terminar em: 7
+#   Vou montar a tabuada de 5 começando em 4 e terminando em 7:
+#   5 X 4 = 20
+#   5 X 5 = 25
+#   5 X 6 = 30
+#   5 X 7 = 35
+#
+#----------------------------------------------------------------
 
-while count <= c:
-    print(a , "X" , count, "=", a*count)
+table = int(input())
+start = int(input())
+end = int(input())
+count = start
+print("Vou montar a tabuada de", table, "começado em", start, "e terminando em", end)
+while count <= end:
+    print(table , "X" , count, "=", table*count)
     count +=1
