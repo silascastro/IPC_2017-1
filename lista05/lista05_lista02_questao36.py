c1e68a7aa38f79426717a4e302a0ef21ce0e3704
natcavalcantex21 <nataliacxavier1@gmail.com>
2017-04-11 20:20:24 -0400

    Update lista05_lista02_questao36.py

--- lista05/lista05_lista02_questao36.py
+++ lista05/lista05_lista02_questao36.py
@@ -1,3 +1,14 @@
+#---------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#Natália Cavalcante Xavier     1715310021
+#Nayara da Silva Cerdeira da Costa 1715310038
+#Marcus Viniciius Paes da Silva Santos 1515070060
+#Matheus de Oliveira Marques 1515310514
+#Lukas Michel Souza Mota 1715310018
+#Escreva  um  algoritmo para  calcular  o  fatorial do  número  N,  cujo 
+#valor é obtido através do usuário pelo teclado
 fat=int(input("Digite um valor "))
 i=1
 nfat=1
