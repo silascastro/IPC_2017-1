9dfca5fa2827b97c91f3a19ded6dceccca850b66
Felipe Almeida <edu468felipe@gmail.com>
2017-04-12 13:25:12 -0400

    Update lista05_lista01_questao04.py

--- lista05/lista05_lista01_questao04.py
+++ lista05/lista05_lista01_questao04.py
@@ -1,9 +1,27 @@
+#----------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Evandro Padilha Barroso Filho     1715310009 
+# Gabriel Barroso da Silva Lima     1715310011
+# Frederico Victor Alfaia Rodrigues 1515200030 
+# Felipe Eduardo Silva de Almeida   1715310031 
+# Felipe Guerreiro De Mello         1315120052 
+# Silas castro de Mendonça          1715310066
+#
+# Supondo que a população de um país A seja da ordem de 80000 habitantes 
+# com uma taxa anual de crescimento de 3% e que a população de B seja 200000 
+# habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule 
+# e escreva o número de anos necessários para que a população do país A ultrapasse 
+# ou iguale a população do país B, mantidas as taxas de crescimento.
+#----------------------------------------------------------------
+
 pop_a = 80000
 grow_a = 0.03
 pop_b = 200000
 grow_b = 0.015
 c = 0
-while pop_a < pop_b:
+while pop_a <= pop_b:
     pop_a = pop_a + (pop_a * grow_a)
     pop_b = pop_b + (pop_b * grow_b)
     c = c + 1
