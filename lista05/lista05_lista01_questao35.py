62cfaff7610f11b3439106851147d3145f7b824a
Evandro Padilha Barroso Filho <evandro7047301@gmail.com>
2017-04-11 21:42:57 -0400

    Update lista05_lista01_questao35.py

--- lista05/lista05_lista01_questao35.py
+++ lista05/lista05_lista01_questao35.py
@@ -8,7 +8,7 @@
 # Felipe Eduardo Silva de Almeida   1715310031 
 # Felipe Guerreiro De Mello         1315120052 
 # Silas castro de Mendonça          1715310066
-# Encontrar números primos é uma tarefa difícil. Faça um programa que gera uma lista
+# 35.Encontrar números primos é uma tarefa difícil. Faça um programa que gera uma lista
 # dos números primos existentes entre 1 e um número inteiro informado pelo usuário.
 
 lim = int(input())
