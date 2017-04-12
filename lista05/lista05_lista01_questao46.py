3befc7c952a125319a3227ae865ff550808c8efe
marcuspaess <mvpdss.eng@uea.edu.br>
2017-04-12 09:54:37 -0400

    Correção da questão

--- lista05/lista05_lista01_questao46.py
+++ lista05/lista05_lista01_questao46.py
@@ -15,45 +15,45 @@
 # e depois informe a média dos saltos conforme a descrição acima informada(retirar o melhor e o pior salto e depois calcular a média).
 # Faça uso de uma lista para armazenar os saltos. Os saltos são informados na ordem da execução, portanto não são ordenados.
 #  O programa deve ser encerrado quando não for informado o nome do atleta.
-salto1 = float(input("Digite o valor do primeiro salto "))
-salto2 = float(input("Digite o valor do segundo salto "))
-salto3 = float(input("Digite o valor do terceiro salto "))
-salto4 = float(input("Digite o valor do quarto salto "))
-salto5 = float(input("Digite o valor do quinto salto "))
-nome=input("Infome o nome do atleta")
-while nome != "":
-    if salto1 < salto2 and salto1 < salto3 and salto1 < salto4 and salto1 < salto5:
-     print("menor salto",salto1)
-     salto1=0
-    elif salto2 < salto1 and salto2 < salto3 and salto2 < salto4 and salto2 < salto5:
-     print("menor salto",salto2)
-     salto2=0
-    elif salto3 < salto1 and salto3 < salto2 and  salto3 < salto4 and salto3 < salto5:
-     print("menor salto",salto3)
-     salto3=0
-    elif salto4 < salto1 and salto4 < salto2 and salto4 < salto3 and salto4 < salto5:
-     print("menor salto",salto4)
-     salto4=0
-    elif salto5 < salto1 and salto5 < salto2 and salto5 < salto3 and salto5 < salto4:
-     print("menor salto",salto5)
-     salto5=0
+athlete = input("Atleta: ")
+jump1 = float(input("Primeiro Salto: "))
+jump2 = float(input("Segundo Salto: "))
+jump3 = float(input("Terceiro Salto: "))
+jump4 = float(input("Quarto Salto: "))
+jump5 = float(input("Quinto salto: "))
 
-    if salto1 > salto2 and salto1 > salto3 and salto1 > salto4 and salto1 > salto5:
-     print("maior salto",salto1)
-     salto1=0
-    elif salto2 > salto1 and salto2 > salto3 and salto2 > salto4 and salto2 > salto5:
-        print("maior salto",salto2)
-        salto2=0
-    elif salto3 > salto1 and salto3 > salto2 and  salto3 > salto4 and salto3 > salto5:
-        print("maior salto",salto3)
-        salto3=0
-    elif salto4 > salto1 and salto4 > salto2 and salto4 > salto3 and salto4 > salto5:
-        print("maior salto",salto4)
-        salto4=0
-    elif salto5 > salto1 and salto5 > salto2 and salto5 > salto3 and salto5 > salto4:
-        print("maior salto",salto5)
-    salto5=0
 
-    media=salto1+salto2+salto3+salto4+salto5/2
+if jump1 > jump2 and jump1 > jump3 and jump1 > jump4 and jump1 > jump5:
+    print("Maior = ", jump1)
+    higher = jump1
+elif jump2 > jump1 and jump2 > jump3 and jump2 > jump4 and jump2 > jump5:
+    print("Maior = ", jump2)
+    higher = jump2
+elif jump3 > jump1 and jump3 > jump2 and jump3 > jump4 and jump3 > jump5:
+    print("Maior = ", jump3)
+    higher = jump3
+elif jump4 > jump1 and jump4 > jump2 and jump4 > jump3 and jump4 > jump5:
+    print("Maior = ", jump4)
+    higher = jump4
+elif jump5 > jump1 and jump5 > jump2 and jump5 > jump3 and jump5 > jump4:
+    print("Maior = ", jump5)
+    higher = jump5
 
-    print("Média dos saltos",media)
+if jump1 < jump2 and jump1 < jump3 and jump1 < jump4 and jump1 < jump5:
+    print("Menor = ", jump1)
+    lower = jump1
+elif jump2 < jump1 and jump2 < jump3 and jump2 < jump4 and jump2 < jump5:
+    print("Menor = ", jump2)
+    lower = jump2
+elif jump3 < jump1 and jump3 < jump2 and jump3 < jump4 and jump3 < jump5:
+    print("Menor = ", jump3)
+    lower = jump3
+elif jump4 < jump1 and jump4 < jump2 and jump4 < jump3 and jump4 < jump5:
+    print("Menor = ", jump4)
+    lower = jump4
+elif jump5 < jump1 and jump5 < jump2 and jump5 < jump3 and jump5 < jump4:
+    print("Menor = ", jump5)
+    lower = jump5
+
+media = (jump1+jump2+jump3+jump4+jump5-higher-lower)/3
+print("A média dos demais saltos do atleta é ", media)
