dea53b31bac79cec01deeb06ab881606d5904a1d
reinaldo22 <vargas.reynaldo22@gmail.com>
2017-04-12 12:40:40 -0400

    Update lista05_lista02_questao36.py

--- lista05/lista05_lista02_questao36.py
+++ lista05/lista05_lista02_questao36.py
@@ -7,6 +7,7 @@
 #Marcus Viniciius Paes da Silva Santos 1515070060
 #Matheus de Oliveira Marques 1515310514
 #Lukas Michel Souza Mota 1715310018
+
 #Escreva  um  algoritmo para  calcular  o  fatorial do  número  N,  cujo 
 #valor é obtido através do usuário pelo teclado
 fat=int(input("Digite um valor "))
