31edadd65bb0a798a29cd5a76bc0123897b7d4dd
Gabriel Queiroz <gabriel.dequeirozsousa@gmail.com>
2017-04-12 17:38:23 -0400

    Update lista05_lista02_questao50.py

--- lista05/lista05_lista02_questao50.py
+++ lista05/lista05_lista02_questao50.py
@@ -1,3 +1,20 @@
+# -----------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Gabriel Nascimento de Oliveira    1715310052
+# Gabriel de Queiroz Souza          1715310044
+# Guilherme Silva de Oliveira       1715310034
+# Hugo Thadeu Silva Cardoso         1715310013
+# Jandinne Duarte de Oliveira       1015070265
+# Joelson Pereira Lima              1715310060
+#
+# Faça um algoritmo que:
+# - leia um número real X do teclado;
+# - determine e imprima o seguinte somatório:
+# Usando os 20 primeiros termos da série.
+# -----------------------------------------------------------------------------------------
 num = float(input("Informe valor: "))
 cont = 19
 val = 1
@@ -21,3 +38,4 @@ while cont > 0:
     val = 1
     fat = aux
     cont = cont - 1
+    print("\n")
