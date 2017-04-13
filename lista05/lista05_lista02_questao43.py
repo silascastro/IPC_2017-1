54670530f103e84d01b82d363f5c8bb4140ae123
reinaldo22 <vargas.reynaldo22@gmail.com>
2017-04-12 22:49:22 -0400

    Update lista05_lista02_questao43.py

--- lista05/lista05_lista02_questao43.py
+++ lista05/lista05_lista02_questao43.py
@@ -1,5 +1,5 @@
 #----------------------------------------------------------------
-# IntroduÁ„o a ProgramaÁ„o de Computadores - IPC
+# Introdu√ß√£o a Programa√ß√£o de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
 # Evandro Padilha Barroso Filho     1715310009
@@ -7,17 +7,18 @@
 # Frederico Victor Alfaia Rodrigues 1515200030
 # Felipe Eduardo Silva de Almeida   1715310031
 # Felipe Guerreiro De Mello         1315120052
-# Silas castro de MendonÁa          1715310066
-# A sÈrie de FETUCCINE È gerada da seguinte forma: 
-# os dois primeiros termos s„o fornecidos pelo usu·rio; 
-# a partir daÌ, os termos s„o gerados com a soma ou 
-# subtraÁ„o dos dois termos anteriores, ou seja:
-# A(i) = A(i-1) + A(i-2) para i Ìmpar
+# Silas castro de Mendon√ßa          1715310066
+#
+# A s√©rie de FETUCCINE √© gerada da seguinte forma: 
+# os dois primeiros termos s√£o fornecidos pelo usu√°rio; 
+# a partir da√≠, os termos s√£o gerados com a soma ou 
+# subtra√ß√£o dos dois termos anteriores, ou seja:
+# A(i) = A(i-1) + A(i-2) para i √≠mpar
 # A(i) = A(i-1) - A(i-2) para i par
 # Criar um algoritmo em PORTUGOL que imprima 
-# os N primeiros termos da sÈrie de FETUCCINE, 
-# sabendo-se que para existir esta sÈrie ser„o 
-# necess·rios pelo menos trÍs termos.
+# os N primeiros termos da s√©rie de FETUCCINE, 
+# sabendo-se que para existir esta s√©rie ser√£o 
+# necess√°rios pelo menos tr√™s termos.
 
 termo1 = int(input("primeiro termo: "))
 termo2 = int(input("segundo termo: "))
