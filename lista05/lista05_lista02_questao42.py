dd8fe0f05ba719e51cdc4db35ce3ca092725a49c
GnascimentoSI <nascimentoUEA@hotmail.com>
2017-04-12 22:46:23 -0400

    Update lista05_lista02_questao42.py
    Roubaram a minha questão, só estou pegando de volta! Código diferente

--- lista05/lista05_lista02_questao42.py
+++ lista05/lista05_lista02_questao42.py
@@ -3,12 +3,12 @@
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
 #
-# Evandro Padilha Barroso Filho     1715310009
-# Gabriel Barroso da Silva Lima     1715310011
-# Frederico Victor Alfaia Rodrigues 1515200030
-# Felipe Eduardo Silva de Almeida   1715310031
-# Felipe Guerreiro De Mello         1315120052
-# Silas castro de Mendonça          1715310066
+# Gabriel Nascimento de Oliveira    1715310052
+# Gabriel de Queiroz Souza          1715310044
+# Guilherme Silva de Oliveira       1715310034
+# Hugo Thadeu Silva Cardoso         1715310013
+# Jandinne Duarte de Oliveira       1015070265
+# Joelson Pereira Lima              1715310060
 #
 # 42) A série de RICCI difere da série de FIBONACCI porque os dois primeiros termos são
 # fornecidos pelo usuário. Os demais termos são gerados da mesma forma que a série de
@@ -18,19 +18,19 @@
 #
 #----------------------------------------------------------------
 
-prev_number = int(input("Digite um numero: "))
-next_number = int(input("Digite outro numero: "))
-fibonacci = 0
-count = 1
+contador = 1
+ricci = 0
+soma = 0
+anterior = int(input('escolha o primeiro termo:'))
+proximo = int(input('escolha o segundo termo:'))
+limite = int(input('escolha a quantidade de  termos:'))
 
-n = int(input('Digite a quantidade de números da sequência fibonacci desejados: '))
+while contador <= limite :
+    print(anterior)
+    soma = soma + anterior
+    ricci = anterior + proximo
+    anterior = proximo
+    proximo = ricci
+    contador = contador + 1
 
-while count <= n:
-    if count == 1:
-        print(prev_number, end = ' ')
-    else:
-        print(next_number, end = ' ')
-        fibonacci = prev_number + next_number
-        prev_number = next_number
-        next_number = fibonacci
-    count += 1
+print('a soma dos termos é :',soma)
